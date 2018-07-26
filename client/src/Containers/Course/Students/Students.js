7235c0ea368df29e3a077a50cabdb5f07d123678
okputadora <okputadora@gmail.com>
2018-07-25 20:17:07 -0400

    some styling to the students page

--- client/src/Containers/Course/Students/Students.js
+++ client/src/Containers/Course/Students/Students.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import classes from './students.css';
 import Avatar from '../../../Components/UI/Avatar/Avatar';
+import Button from '../../../Components/UI/Button/Button';
 
 const students = props => {
   console.log(props.notifications)
   const joinRequests = props.notifications.map(ntf => (
     <div className={classes.Ntf}>
-      <Avatar username={ntf.user.username} />
-      {/* <Button click={} */}
+      <div style={{margin: 20}}><Avatar username={ntf.user.username} /></div>
+      <div>requested access to join this course [TIMESTAMP]</div>
+      <Button click={''}>Grant Access</Button>
         </div>
   ))
   console.log(joinRequests)
@@ -17,6 +19,8 @@ const students = props => {
       <div className={classes.Notifications}>
         {joinRequests}
       </div>
+      <h3>Add New Students</h3>
+      <h3>Student List</h3>
     </div>
   )
 }
