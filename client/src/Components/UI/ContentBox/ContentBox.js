1692acd1f4e8fc48322bbab7a57b1aa3b052dafc
okputadora <okputadora@gmail.com>
2018-08-14 19:35:20 -0400

    added icons to contentBoxes

--- client/src/Components/UI/ContentBox/ContentBox.js
+++ client/src/Components/UI/ContentBox/ContentBox.js
@@ -2,19 +2,22 @@
   // Title
   // notifications
   // locked
+  // contentType
 
 import React from 'react';
-import classes from './contentBox.css'
-let alignClass= classes.Center;
+import classes from './contentBox.css';
+import Icons from './Icons/Icons';
 const contentBox = props => {
+  let alignClass = classes.Center;
   if (props.align === 'left') alignClass = classes.Left;
   if (props.align === 'right') alignClass = classes.Right;
   const notifications = (props.notifications > 0) ? <div className={classes.Notification}>{props.notifications}</div> : null;
-  const locked = props.locked ? <div className={classes.Lock}><i className="fas fa-lock"></i></div> : null;
+  // const roomTpe = props.roomType ? <
   return (
     <div className={classes.Container} onClick={this.toggleCollapse}>
+      {Icons}
       {notifications}
-      {locked}
+      <div>Icons</div>
       <div className={classes.Title}>{props.title}</div>
       <div className={[classes.Content, alignClass].join(' ')}>
         {props.children}
