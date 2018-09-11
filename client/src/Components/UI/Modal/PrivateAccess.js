7331929fae409502674e337fa1ad00873257b4e0
okputadora <okputadora@gmail.com>
2018-09-11 13:29:54 -0400

    fixed fetch bug

--- client/src/Components/UI/Modal/PrivateAccess.js
+++ client/src/Components/UI/Modal/PrivateAccess.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Modal from './Modal';
 import Button from '../Button/Button';
-const privateAccess = props => {
-  return (
-    <Modal show={true}>
-      <p>You currently don't have access to this course. If you would like to
-        request access from the owner click "Join". When your request is accepted
-        this course will appear in your list of courses on your profile.
-      </p>
-      <Button click={props.requestAccess}>Join</Button>
-    </Modal>
-  )
+import { withRouter } from 'react-router-dom';
+class privateAccess extends Component {
+
+  state = {
+    show: true
+  }
+
+  closeModal = () => {
+    this.setState({show: false})
+    this.props.history.goBack();
+  }
+
+  render(){
+    return (
+      <Modal show={this.state.show} closeModal={this.closeModal}>
+        <p>You currently don't have access to this course. If you would like to
+          request access from the owner click "Join". When your request is accepted
+          this course will appear in your list of courses on your profile.
+        </p>
+        <Button click={this.props.requestAccess}>Join</Button>
+      </Modal>
+    )
+  }
 }
 
-export default privateAccess;
+export default withRouter(privateAccess);
