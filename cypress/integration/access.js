86a9c2e1a5826bea6e3841aa0c763b79dfc337d8
okputadora <okputadora@gmail.com>
2018-09-20 09:45:41 -0400

    successfully seed db from cypress befoe hook

--- cypress/integration/access.js
+++ cypress/integration/access.js
@@ -1,3 +1,5 @@
+// const
+const exec = require('child_process').exec
 const user = require('../fixtures/user')
 const course = require('../fixtures/course')
 const room = require('../fixtures/room')
@@ -5,10 +7,15 @@ const activity = require('../fixtures/activity')
 
 describe('test access requests', function(){
   before(function(){
+    cy.task('seedDB')
+    // spawn('md-seed run --dropdb')
+    /// cy.exec DOESNT WORK ON WINDOWS 😫
     // cy.exec('').then(res => console.log('promise resolved'))
+
   })
 
   it("user requests access to a course", function(){
-    cy.visit('/')
+    cy.visit('/publicList/rooms')
+
   })
 })
