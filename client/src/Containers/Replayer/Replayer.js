819d2bab5a2b380d1f3934a18cc04910f1a7a6db
okputadora <okputadora@gmail.com>
2018-09-21 11:09:45 -0400

    divided up duration into blocks of activity

--- client/src/Containers/Replayer/Replayer.js
+++ client/src/Containers/Replayer/Replayer.js
@@ -22,19 +22,38 @@ class Replayer extends Component {
     .sort((a, b) => a.timeStamp - b.timeStamp);
   startTime = moment
     .unix(this.log[0].timeStamp / 1000)
-    .format('MM/DD/YYYY h:mm:s A');
+    .format('MM/DD/YYYY h:mm:ss A');
   endTime = moment
     .unix(this.log[this.log.length - 1].timeStamp / 1000)
-    .format('MM/DD/YYYY h:mm:s A');
+    .format('MM/DD/YYYY h:mm:ss A');
+  blocks = [];
+  blockStart = {
+    unix: this.log[0].timeStamp / 1000,
+    time: moment.unix(this.log[0].timeStamp / 1000).format('MM/DD/YYYY h:mm:ss A'),
+    logIndex: 0
+  };
   displayDuration = this.log.reduce((acc, cur, idx, src) => {
     if (src[idx + 1]){
       let diff = src[idx + 1].timeStamp - cur.timeStamp
       if (diff < maxWait) {
         acc += diff
+      } else {
+        let newBlock = {
+          startTime: this.blockStart.time,
+          endTime: moment.unix(cur.timeStamp /1000).format('MM/DD/YYYY h:mm:ss A'),
+          duration: (cur.timeStamp / 1000)  - this.blockStart.unix,
+          startIndex: this.blockStart.logIndex,
+          endIndex: idx,
+        }
+        this.blocks.push(newBlock)
+        this.blockStart = {
+          unix: src[idx + 1].timeStamp/1000,
+          time: moment.unix(src[idx + 1].timeStamp / 1000).format('MM/DD/YYYY h:mm:ss A'),
+          logIndex: idx + 1}
       }
     }
     return acc;
-  }, 0)
+  }, 0) / 1000;
 
 
   componentDidUpdate(prevProps, prevState){
@@ -58,6 +77,9 @@ class Replayer extends Component {
   }
 
   render() {
+    console.log(this.blocks)
+    console.log(this.log.map(ent => moment.unix(ent.timeStamp / 1000).format('MM/DD/YYYY h:mm:ss A')))
+    console.log(this.displayDuration)
     const { room } = this.props
     const event = this.log[this.state.logIndex];
     return (
@@ -78,6 +100,7 @@ class Replayer extends Component {
             pausePlay={this.pausePlay}
             index={this.state.logIndex}
             displayDuration={this.displayDuration}
+            blocks={this.blocks}
             startTime={this.startTime}
             event={event}
             endTime={this.endTime}
