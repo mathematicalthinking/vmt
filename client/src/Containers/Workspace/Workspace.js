8c7fe66f69c29ed239da25b8505eb6f2c7f384af
okputadora <okputadora@gmail.com>
2018-09-22 10:32:05 -0400

    autogenerate messages when users enter/exit a room

---
+++ client/src/Containers/Workspace/Workspace.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import io from 'socket.io-client';
+import * as actions from '../../store/actions';
+import WorkspaceLayout from '../../Layout/Room/Workspace/Workspace';
+import DesmosGraph from './DesmosGraph';
+import GgbGraph from './GgbGraph';
+import Chat from './Chat';
+// import Replayer from ''
+class Workspace extends Component {
+
+  socket = io.connect(process.env.REACT_APP_SERVER_URL);
+  
+  componentDidMount() {
+    const { updateRoom, room, user} = this.props;
+    const sendData = {
+      userId: user.id,
+      roomId: room._id,
+      username: user.username,
+      roomName: room.name,
+    }
+    this.socket.emit('JOIN', sendData, (res, err) => {
+      if (err) {
+        console.log(err) // HOW SHOULD WE HANDLE THIS
+      }
+      // WE COULD MAKE TAKE THIS OUT OF THE CALLBACK AND UPDATE IMMEDIATELY
+      // AND THEN NOTIFY THE USER OF AN ERROR ONLY IF SOMETHING GOES WRONG
+      const updatedUsers = [...room.currentUsers, {_id: user.id, username: user.username}]
+      updateRoom(room._id, {currentUsers: updatedUsers})
+    })
+
+    this.socket.on('USER_JOINED', data => {
+      updateRoom(room._id, {currentUsers: data.currentUsers})
+    })
+
+    this.socket.on('USER_LEFT', data => {
+      updateRoom(room._id, {currentUsers: data.currentUsers})
+    })
+  }
+
+  componentWillUnmount () {
+    const { updateRoom, room, user} = this.props;
+    const data = {
+      userId: user.id,
+      roomId: room._id,
+      username: user.username,
+      roomName: room.name,
+    }
+    this.socket.emit('LEAVE', data, (res) => {
+      updateRoom(room._id, {currentUsers: room.currentUsers.filter(u => u._id !== user.id)})
+      this.socket.disconnect();
+    })
+  }
+
+  render() {
+    const { room, user } = this.props;
+    console.log("current users", room.currentUsers)
+    return (
+      <WorkspaceLayout
+        members = {room.currentUsers}
+        graph = {room.roomType === 'geogebra' ?
+          // I dont like that these need to be wrapped in functions 👇 could do
+          // props.children but I like naming them.
+          () => <GgbGraph room={room} socket={this.socket} user={user} /> :
+          () => <DesmosGraph  room={room} socket={this.socket} user={user} />}
+        chat = {() => <Chat roomId={room._id} messages={room.chat || []} socket={this.socket} user={user} />}
+      />
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    room: state.rooms.byId[ownProps.match.params.room_id],
+    user: state.user,
+    loading: state.loading.loading,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    updateRoom: (roomId, body) => dispatch(actions.updateRoom(roomId, body)),
+  }
+}
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Workspace);
---
+++ sockets.js
@@ -0,0 +1,83 @@
+const controllers = require('./controllers')
+
+const sockets = {};
+sockets.init = server => {
+
+    const io = require('socket.io')(server, {wsEngine: 'ws'}); // AHHHH FUCK YOU WINDOWS
+    // without this wsEngine setting the socket events would be delayed upwards of 20 seconds // NOT SURE IF MAC DEVELOPERS WILL NEED TO REMOVE THIS
+    io.sockets.on('connection', socket => {
+      socket.on('JOIN', (data, callback) => {
+        socket.join(data.roomId, () => {
+          // update current users of this room
+          const promises = [];
+          const message = {
+            user: {_id: data.userId, username: 'VMTbot'},
+            roomId: data.roomId,
+            text: `${data.username} joined ${data.roomName}`,
+            timestamp: new Date().getTime(),
+          }
+          promises.push(controllers.message.post(message))
+
+          promises.push(controllers.room.addCurrentUsers(data.roomId, data.userId))
+          Promise.all(promises)
+          .then(results => {
+            io.in(data.roomId).emit('RECEIVE_MESSAGE', message)
+            socket.broadcast.to(data.roomId).emit('USER_JOINED', {currentUsers: results[1].currentUsers, message,});
+            callback({result: results[1].currentUsers}, null)
+          })
+          .catch(err => callback(null, err))
+        })
+      });
+      socket.on('LEAVE', (data, callback) => {
+        socket.leave(data.roomId, () => {
+          const promises = [];
+          const message = {
+            user: {_id: data.userId, username: 'VMTbot'},
+            roomId: data.roomId,
+            text: `${data.username} left ${data.roomName}`,
+            timestamp: new Date().getTime(),
+          }
+          promises.push(controllers.message.post(message))
+          promises.push(controllers.room.removeCurrentUsers(data.roomId, data.userId))
+          Promise.all(promises)
+          .then(results => {
+            console.log("room after disconnect: ",results[1]);
+            io.in(data.roomId).emit('RECEIVE_MESSAGE', message)
+            socket.broadcast.to(data.roomId).emit('USER_LEFT', {currentUsers: results[1].currentUsers, message,});
+            callback({result: room.currentUsers})
+          })
+          .catch(err => console.log(err))
+        })
+      })
+      socket.on('SEND_MESSAGE', (data, callback) => {
+        const postData = {...data}
+        postData.user = postData.user._id;
+        controllers.message.post(postData)
+        .then(res => {
+          socket.broadcast.to(data.room).emit('RECEIVE_MESSAGE', data);
+          callback('success', null)
+        })
+        .catch(err => {
+          callback('fail', err)
+          console.log(err)
+        })
+        // broadcast new message
+      })
+
+      socket.on('SEND_EVENT', (data) => {
+        // console.log('receiving event: ', data)
+        // console.log(typeof data.event)
+        // console.log(io.sockets.clients(data.roomId))
+        if (typeof data.event !== 'string') {
+          data.event = JSON.stringify(data.event)
+        }
+        controllers.event.post(data) // @TODO set this up as middleware ?????
+        socket.broadcast.to(data.room).emit('RECEIVE_EVENT', data)
+        // callback('success')
+        console.log('success')
+      })
+    });
+
+}
+
+module.exports = sockets;
