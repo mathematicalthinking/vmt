dd080bb52029e5756a8cdf0c92df7de8ed1f2e03
okputadora <okputadora@gmail.com>
2018-09-24 11:44:26 -0400

    i was grabing the wrong interval

--- client/src/Components/Replayer/Clock.js
+++ client/src/Components/Replayer/Clock.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import classes from './replayer.css';
+import moment from 'moment';
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
@@ -29,8 +30,9 @@ class Clock extends PureComponent {
         hours++;
       }
     }
-    let time = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-    this.setState({absTime: time,})
+    const absTime = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+    const relTime = moment(this.state.relTime).add(1, 'second').format('MM/DD/YYYY h:mm:ss A')
+    this.setState({absTime, relTime,})
   }
 
   render() {
