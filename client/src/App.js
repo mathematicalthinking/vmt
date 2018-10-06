826b12c19d683d5a91c30eb8c8d132f775c7eb47
okputadora <okputadora@gmail.com>
2018-10-06 11:28:09 -0400

    fixed myVMT route

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,13 @@ import MyVmt from './Routes/MyVmt';
 import Home from './Routes/Home';
 import { Provider } from 'react-redux';
 import configureStore from './configureStore';
-import { Login} from './Containers';
 
 const store = configureStore();
 const App = props => (
   <Provider store={store}>
     <Router >
       <Switch>
-        <Route path={'/'} component={Home} />
+        <Route exact path={'/'} component={Home} />
         <Route path={'/myVMT'} component={MyVmt} />
       </Switch>
     </Router>
