56b1b3d58358e01d4195216bcd21dcc9f0f81caf
okputadora <okputadora@gmail.com>
2018-10-08 11:22:14 -0400

    changed publiclist to community

--- client/src/Layout/index.js
+++ client/src/Layout/index.js
@@ -1,3 +1,4 @@
 export { default as HomepageLayout } from './Homepage/Homepage';
 export { default as LoginLayout } from './Login/Login';
 export { default as Signup } from './Signup/Signup';
+export { default as CommunityLayout } from './Community/Community';
