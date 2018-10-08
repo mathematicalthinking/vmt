a1638edeb0c218416016f8b9304f88c12aeef284
okputadora <okputadora@gmail.com>
2018-10-08 10:54:45 -0400

    new create options for different resources

--- client/src/Components/Navigation/HomeNav/HomeNav.js
+++ client/src/Components/Navigation/HomeNav/HomeNav.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import NavItem from '../NavItem/NavItem';
 import { Link } from 'react-router-dom';
+import Aux from '../../HOC/Auxil';
 import classes from './homeNav.css';
 const navbar = (props) => {
+  console.log("NAVBAR PROPS: ", props)
   return (
     <nav className={classes.Nav}>
       <div className={classes.NavContainer}>
         <div className={classes.Logo}><Link to='/'>Logo</Link></div>
         <ul className={classes.NavList}>
-          <NavItem link='/login' name='Login' />
-          <NavItem link='/signup' name='Signup' />
+          {props.loggedIn ? <NavItem link='/myVMT/courses' name='My VMT' /> :
+          <Aux>
+            <NavItem link='/login' name='Login' />
+            <NavItem link='/signup' name='Signup' />
+          </Aux>}
           <NavItem link='/about' name='About' />
           <NavItem link='/tutorials' name='Tutorials' />
         </ul>
@@ -18,4 +24,10 @@ const navbar = (props) => {
   )
 }
 
-export default navbar;
+
+const mapStateToProps = (store) => ({
+  loggedIn: store.user.loggedIn,
+})
+
+export default connect(mapStateToProps, null)(navbar);
+
