e83e7a67298972d5e2ad5b4f2995d7cba750b390
okputadora <okputadora@gmail.com>
2018-10-10 11:18:45 -0400

    changed build config

--- app.js
+++ app.js
@@ -41,19 +41,19 @@ app.engine('handlebars', exphbs({defaultLayout: 'ggbMain'}));
 app.set('view engine', 'handlebars');
 
 //serve react files in a production enviornment
-if (process.env.NODE_ENV === 'test') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-}
+// if (process.env.NODE_ENV === 'test') {
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+// }
 
 
 app.use(express.static(path.join(__dirname, 'client/public')));
 
 app.get('/', (req, res) => {
-  if (process.emit.NODE_ENV === 'test') {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'))
-  } else {
+  // if (process.emit.NODE_ENV === 'test') {
+  //   res.sendFile(path.join(__dirname, 'client/build/index.html'))
+  // } else {
     res.sendFile(path.join(__dirname, '/client/public/index.html'));
-  }
+  // }
 });
 
 // app.get('/ggb', (req, res) => {
