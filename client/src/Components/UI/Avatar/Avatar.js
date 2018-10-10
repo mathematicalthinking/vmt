68c6a43b44ac45ef50a4d9f7061ac5d3471c8806
okputadora <okputadora@gmail.com>
2018-10-10 09:51:18 -0400

    changed sidepanel layout

--- client/src/Components/UI/Avatar/Avatar.js
+++ client/src/Components/UI/Avatar/Avatar.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import classes from './avatar.css';
 import { Link } from 'react-router-dom';
-const avatar = props => (
+const avatar = props => {
+  let fontSize = 15;
+  let padding = 7;
+  let border = 'none'
+  if (props.size === 'small') fontSize = 10;
+  else if (props.size === 'medium') fontSize = 25;
+  else if (props.size === 'large') {
+    padding = 28;
+    fontSize = 80;
+    border = '3px solid white'
+  } 
+  return (
   <div className={classes.UserInfo}><Link to='/profile'>
-    <i className={["fas fa-user", classes.Avatar].join(' ')}></i>
+    <i className={["fas fa-user", classes.Avatar].join(' ')} style={{fontSize, padding, border,}}></i>
     {props.username}
   </Link></div>
-)
+  )
+}
+
 
 export default avatar;
