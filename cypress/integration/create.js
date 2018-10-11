908b947a5e5bebf274c3273ab6b1232216cfe33b
okputadora <okputadora@gmail.com>
2018-10-11 12:42:10 -0400

    all tests passing - though there is an issue clearing sessionStorage on cypress"s end

--- cypress/integration/create.js
+++ cypress/integration/create.js
@@ -5,12 +5,7 @@ const activity = require('../fixtures/activity')
 
 describe('create each type of resource', function(){
   before(function(){
-    cy.task('seedDBLogin').then(() => {
-      cy.window((win) => {
-        win.sessionStorage.clear()
-        cy.login(user)
-      })
-    })
+    cy.task('seedDBLogin').then(() => {cy.login(user)})
     // cy.visit('/myVMT/courses')
   })
   it('creates a course', function(){
