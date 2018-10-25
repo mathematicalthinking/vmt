b71831db78e063ca034b362278d49411ce09e011
okputadora <okputadora@gmail.com>
2018-10-25 12:39:05 -0400

    added select animation to content box

--- client/src/Components/UI/ContentBox/ContentBox.js
+++ client/src/Components/UI/ContentBox/ContentBox.js
@@ -7,26 +7,46 @@ class ContentBox extends PureComponent{
   
   state = {
     showOverlay: false,
+    selectAnimation: false,
+    selected: false,
   }
   
   hoverOnSelect = () => {
     console.log('mouse enter')
     this.setState({showOverlay: true})
   }
+
+  select = () => {
+    this.setState({selectAnimation: true})
+    setTimeout(this.setState({selected: true}), 400)
+  }
   render() {
+    let { selecting } = this.props;
     let alignClass = classes.Center;
+    let animatedClass = '';
+    let selectedClass = '';
+    let selectedClassPlus = '';
     if (this.props.align === 'left') alignClass = classes.Left;
     if (this.props.align === 'right') alignClass = classes.Right;
+    if (this.state.selectAnimation) animatedClass = classes.Selecting;
+    if (this.state.selected) {
+      selectedClassPlus = classes.SelectedPlus;
+      selectedClass = classes.Selected;
+    };
     const notifications = (this.props.notifications > 0) ? <div className={classes.Notification} data-testid="content-box-ntf">{this.props.notifications}</div> : null;
     return (
     <Aux>
-      <Link 
-        to={this.props.link} 
+      <Link to={selecting ? "#" : this.props.link}>
+      <div 
         data-testid='content-box' 
-        onMouseEnter={this.props.selecting ? this.hoverOnSelect : null} 
-        onMouseLeave={this.props.selecting ? () => {this.setState({showOverlay: false})} : null} >
-      <div className={classes.Container} onClick={this.toggleCollapse}>
-        {this.state.showOverlay ? <div className={classes.SelectOverlay}><i className="fas fa-plus"></i></div> : null}
+        className={[classes.Container, selectedClass].join(" ")} 
+        onClick={selecting ? this.select : null}
+        onMouseEnter={selecting ? this.hoverOnSelect : null}
+        onMouseLeave={selecting ? () => {this.setState({showOverlay: false})} : null}
+      >
+        {this.state.showOverlay ? <div className={classes.SelectOverlay}>
+          <i className={[classes.Plus, "fas fa-plus", animatedClass, selectedClassPlus].join(" ")}></i>
+        </div> : null}
         <div className={classes.Icons}><Icons image={this.props.image} lock={this.props.locked} roomType={this.props.roomType}/></div>
         {notifications}
         <div className={classes.Title} data-testid="content-box-title">{this.props.title}</div>
