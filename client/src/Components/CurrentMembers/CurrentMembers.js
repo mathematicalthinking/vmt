09dd18913bb7a34241707121aa9bc1e2c6236a29
okputadora <okputadora@gmail.com>
2018-11-09 15:31:59 -0500

    added tabs to the UI

--- client/src/Components/CurrentMembers/CurrentMembers.js
+++ client/src/Components/CurrentMembers/CurrentMembers.js
@@ -8,7 +8,7 @@ class CurrentMembers extends Component {
     console.log(members)
     return (
       <div className={classes.Container}>
-        <h3>Current Members</h3>
+        <h3 className={classes.Title}>Current Members</h3>
         <div className={classes.Left} data-testid='current-members'>
           {members ? members.map(user =>
             <div
