92f987de2fba296035f3e180f3e4f358ec6e110b
okputadora <okputadora@gmail.com>
2018-11-16 09:55:56 -0500

    added duedate to room description

--- client/src/Components/HOC/PrivateRoute.js
+++ client/src/Components/HOC/PrivateRoute.js
@@ -4,12 +4,8 @@ import { fail } from '../../store/actions/loading';
 import { Route, Redirect } from 'react-router-dom';
 
 const privateRoute = ({component: Component, authed, redirectPath, fail, ...rest}) => {
-<<<<<<< HEAD
-  if (redirectPath === '/signup' && !authed) {
-=======
   if (redirectPath === '/signup') {
     console.log('fail')
->>>>>>> show error message if user requests access w/o being logged in
     fail('You need to be signed in to access this resource')
   }
   return (
