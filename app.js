f7a486007fd7013240e5c8719d7ff0596e971529
okputadora <okputadora@gmail.com>
2018-11-16 13:20:51 -0500

    fixed!

--- app.js
+++ app.js
@@ -62,14 +62,6 @@ if (process.env.NODE_ENV === 'travistest' || process.env.NODE_ENV === 'productio
 
 
 
-app.get('/*', (req, res) => {
-  // if (process.env.NODE_ENV === 'travistest' || proces.env.NODE_ENV === 'production') {
-    console.log("sending prod version of react")
-    res.sendFile(path.join(__dirname, 'client/build/index.html'))
-  // } else {
-    // res.sendFile(path.join(__dirname, '/client/public/index.html'));
-  // }
-});
 
 
 
@@ -83,9 +75,9 @@ app.use(cookieParser());
 // @TODO remove before going to production
 // app.use(function (req, res, next) {
   //     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  //     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  //     // Request methods you wish to allow
+  //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 //     // Request headers you wish to allow
 //     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 //     // Set to true if you need the website to include cookies in the requests sent
@@ -105,6 +97,14 @@ app.use('/desmos', desmos);
 app.use('/auth', auth);
 app.use('/api', api);
 
+app.get('/*', (req, res) => {
+  // if (process.env.NODE_ENV === 'travistest' || proces.env.NODE_ENV === 'production') {
+    console.log("sending prod version of react")
+    res.sendFile(path.join(__dirname, 'client/build/index.html'))
+  // } else {
+    // res.sendFile(path.join(__dirname, '/client/public/index.html'));
+  // }
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
