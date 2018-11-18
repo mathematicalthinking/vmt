2c5669646662f5450194bdb5d459abd2c0ecda88
okputadora <okputadora@gmail.com>
2018-11-18 14:15:22 -0500

    tests passing with new data fetching strategy

---
+++ client/src/Components/HOC/CheckAccess.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+
+//WE NEED 
+const checkAccess = ({resource, id, userId}) => {
+  // check the store
+
+    // if not in stroe api request 
+  // check access 
+  return (
+
+  )
+}
+
+export default checkAccess;
