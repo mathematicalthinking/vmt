db325b14dd32e230781cc482d75ad957350c679b
okputadora <okputadora@gmail.com>
2018-11-25 13:29:52 -0500

    contentBox smalled and collapsible

--- client/src/Components/UI/ContentBox/ContentBox.js
+++ client/src/Components/UI/ContentBox/ContentBox.js
@@ -3,12 +3,24 @@ import { Link } from 'react-router-dom';
 import classes from './contentBox.css';
 import Icons from './Icons/Icons';
 import Aux from '../../HOC/Auxil';
+import Expand from './expand.js';
+
+
 class ContentBox extends PureComponent{
   
   state = {
     showOverlay: false,
     selectAnimation: false,
     selected: false,
+    expanded: false,
+  }
+
+  toggleExpand = (event) => {
+    event.preventDefault();
+    console.log('toggling expand')
+    this.setState(prevState => ({
+      expanded: !prevState.exapnded
+    }))
   }
   
   hoverOnSelect = () => {
@@ -37,28 +49,39 @@ class ContentBox extends PureComponent{
     const notifications = (this.props.notifications > 0) ? <div className={classes.Notification} data-testid="content-box-ntf">{this.props.notifications}</div> : null;
     return (
     <Aux>
-      <Link to={selecting ? "#" : this.props.link}>
+      <Link 
+        to={selecting ? "#" : this.props.link}
+        className={[classes.Container, selectedClass].join(" ")} 
+        style={{height: this.state.expanded ? 150 : 50}}
+      >
       <div 
         data-testid={`content-box-${this.props.title}`} 
-        className={[classes.Container, selectedClass].join(" ")} 
+        className={classes.SubContainer}
         onClick={selecting ? this.select : null}
         onMouseEnter={selecting ? this.hoverOnSelect : null}
         onMouseLeave={selecting ? () => {this.setState({showOverlay: false})} : null}
       >
-        {this.state.showOverlay ? <div className={classes.SelectOverlay} data-testid={`overlay-${this.props.title}`}>
-          <i className={[classes.Plus, "fas fa-plus", animatedClass, selectedClassPlus].join(" ")}></i>
-        </div> : null}
-        <div className={classes.Icons}><Icons image={this.props.image} lock={this.props.locked} roomType={this.props.roomType}/></div>
+        {this.state.showOverlay ? 
+          <div className={classes.SelectOverlay} data-testid={`overlay-${this.props.title}`}>
+            <i className={[classes.Plus, "fas fa-plus", animatedClass, selectedClassPlus].join(" ")}></i>
+          </div> 
+        : null}
         {notifications}
-        <div className={classes.Title} data-testid="">{this.props.title}</div>
+        <div className={classes.TitleIcons}>
+          <div className={classes.Icons}>
+            <Icons image={this.props.image} lock={this.props.locked} roomType={this.props.roomType}/>
+          </div>
+          <div className={classes.Title} data-testid="">{this.props.title}</div>
+        </div>
         <div className={[classes.Content, alignClass].join(' ')}>
           {/* // Maybe separate all of this out ot its own component or revert back passing in this.props.children */}
-          {this.props.details ?
-            <div>
+          {this.props.details && this.state.expanded ?
+            <div className={classes.Expanded}>
               <div>{this.props.details.description || ''}</div>
               {this.props.details.facilitators.length > 0 ? <div>Facilitators: {this.props.details.facilitators.map(facilitator => facilitator)}</div> : null}
               {this.props.details.entryCode ? <div>Entry Code: {this.props.details.entryCode}</div> : null}
-            </div> : this.props.children}
+            </div> 
+          : <div className={classes.Expand}><Expand clickHandler={this.toggleExpand}/></div>}
         </div>
       </div>
       </Link>
---
+++ client/src/Components/UI/ContentBox/expand.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const expandIcon = ({clickHandler, height, width, viewBox}) => {
+  return (
+    <svg onClick={clickHandler} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/>
+      <path d="M0 0h24v24H0z" fill="none"/>
+    </svg>
+  )
+}
+
+export default expandIcon;
