39b9d420d2bfee0312209957bdbc1c6bd9f8a086
okputadora <okputadora@gmail.com>
2018-11-26 13:43:09 -0500

    fixed missing merge fix

--- client/src/Layout/BoxList/BoxList.js
+++ client/src/Layout/BoxList/BoxList.js
@@ -7,61 +7,6 @@ const boxList = props => {
   let listElems = "There doesn't appear to be anything here yet";
   if (props.list.length > 0) {
     listElems = props.list.map((item, i) => {
-<<<<<<< HEAD
-      let notifications = 0;
-      let details = undefined;
-      if (props.listType === 'private') {
-        if (props.notifications.length > 0) {
-          props.notifications.forEach((ntf) => {
-            if (ntf._id === item._id || ntf.room === item._id) {
-              notifications += 1;
-            }
-          })
-        }
-        details = {
-          entryCode: item.entryCode,
-          description: item.description,
-          facilitators: [],
-        }
-      } else {
-        details = {
-          facilitators: item.members ? item.members.reduce((acc, member) => {
-
-            if (member.role === 'facilitator') acc.push(member.user.username);
-            return acc;
-          }, []) : []
-        }
-      }
-      return (
-        <div className={classes.ContentBox} key={i}>
-          {!props.draggable ? <ContentBox
-            title={item.name}
-            link={`${props.linkPath}${item._id}${props.linkSuffix}`}
-            key={item._id}
-            id={item._id}
-            image={item.image}
-            notifications={notifications}
-            roomType={item.roomType}
-            locked={!item.isPublic} // @TODO Should it appear locked if the user has access ? I can see reasons for both
-            details={details}
-            selecting={props.selecting}
-            select={props.select}
-                              >
-            {item.description}
-          </ContentBox> :
-          <DragContentBox
-            title={item.name}
-            link={`${props.linkPath}${item._id}${props.linkSuffix}`}
-            key={item._id}
-            id={item._id}
-            notifications={notifications}
-            roomType={item.roomType}
-            resource={props.resource}
-            locked={!item.isPublic} // @TODO Should it appear locked if the user has access ? I can see reasons for both
-            details={details}
-          >
-          </DragContentBox>
-=======
       if (item) {
         console.log("ITEM: ", item)
         let notifications = 0;
@@ -73,7 +18,6 @@ const boxList = props => {
                 notifications += 1;
               }
             })
->>>>>>> new way of fetching data
           }
           details = {
             entryCode: item.entryCode,
