31fb7433c654e568f20730c4bc7e2d0d0f51857e
Daniel Kelly <djk8552@gmail.com>
2018-12-14 15:28:57 -0500

    Refactor api routes middleware and error handling

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const controllers = require('../controllers')
+const middleware = require('../middleware/api');
+const errors = require('../middleware/errors');
 
-const defaultError = {
-  confirmation:'fail',
-  message:'Invalid resource...check your spelling'
-}
+router.param('resource', middleware.validateResource)
+router.param('id', middleware.validateId);
 
 router.get('/:resource', (req, res, next) => {
-	let resource = req.params.resource;
-	let controller = controllers[resource];
-	if (controller == null){
-		return res.status(400).json(defaultError)
-	}
-	controller.get(req.query.params).then(results => {
-		res.json({
-	    confirmation: 'success',
-	    results: results
-	  })
-	})
-	.catch(err => {
-		res.status(404).json({
-			confirmation: 'fail',
-			errorMessage: err
-		})
-	})
+	const controller = controllers[req.params.resource];
+
+  controller.get(req.query.params)
+    .then(results => res.json({ results }))
+	  .catch(err => errors.sendError.InternalError(null, res))
 })
 
 // router.get('/:resource/ids', (req, res, next) => {
@@ -47,94 +35,57 @@ router.get('/:resource', (req, res, next) => {
 // 	})
 // })
 
-router.get('/:resource/:id', (req, res, next) => {
-	let resource = req.params.resource
-	let id = req.params.id
-	let controller  = controllers[resource]
-	if (controller == null){
-		res.status(400).json(defaultError)
-	}
+router.get('/:resource/:id', middleware.validateUser, (req, res, next) => {
+  const { id, resource } = req.params.id;
+	const controller = controllers[resource];
+
 	controller.getById(id)
-	.then(result => {
-		res.json({
-			confirmation: 'success',
-			result: result
-		})
-	})
-	.catch(err => {
-		res.status(400).json({
-			confirmation: 'fail',
-			message: err
-		})
-	})
+	  .then(result => res.json({ result }))
+	  .catch(err => errors.sendError.InternalError(null, res))
 })
 
-router.post('/:resource', (req, res, next) => {
-	let resource = req.params.resource;
-	let controller = controllers[resource]
-	if (controller == null) return res.status(400).json(defaultError);
+router.post('/:resource', middleware.validateUser, (req, res, next) => {
+	const controller = controllers[req.params.resource]
+
 	controller.post(req.body)
-	.then(result => res.json({confirmation: 'success', result,}))
-	.catch(err => res.status(400).json({confirmation: 'fail', message: err}))
+	  .then(result => res.json({ result }))
+	  .catch(err => errors.sendError.InternalError(null, res))
 })
 
-router.put('/:resource/:id/add', (req, res, next) => {
-	let { resource, id, } = req.params;
-	let controller = controllers[resource];
-	if (controller === null) return res.status(400).json(defaultError)
-	controller.add(id, req.body)	
-	.then(result => {
-		console.log("RESULT: ", console.log(result))
-		res.json(result)
-	})
-	.catch((err) => res.status(400).json({confirmation: 'fail', message: err})) 
+router.put('/:resource/:id/add', middleware.validateUser, (req, res, next) => {
+	const { resource, id } = req.params;
+	const controller = controllers[resource];
+
+	controller.add(id, req.body)
+    .then(result => res.json({ result }))
+    .catch(err => errors.sendError.InternalError(null, res))
 })
 
-router.put('/:resource/:id/remove', (req, res, next) => {
-	let { resource, id, } = req.params;
-	let controller = controllers[resource];
-	controller.remove(id, req.body)
-	.then(result => res.json(result))
-	.catch((err) => res.status(400).json({confirmation: 'fail', message: err})) 
+router.put('/:resource/:id/remove', middleware.validateUser, (req, res, next) => {
+	const { resource, id } = req.params;
+	const controller = controllers[resource];
+
+  controller.remove(id, req.body)
+    .then(result => res.json(result))
+    .catch(err => errors.sendError.InternalError(null, res))
 })
 
-router.put('/:resource/:id', (req, res, next) => {
-	let resource = req.params.resource;
-	let controller = controllers[resource];
-  if (controller == null){
-		return res.json(defaultError)
-	}
-  controller.put(req.params.id, req.body)
-  .then(result => {
-    res.json({confirmation: 'success', result,});
-  })
-  .catch(err => {
-    res.status(400).json({
-      confirmation: 'fail',
-      message: err,
-    })
-  })
+router.put('/:resource/:id', middleware.validateUser, (req, res, next) => {
+	const { resource, id } = req.params;
+	const controller = controllers[resource];
+
+  controller.put(id, req.body)
+    .then(result => res.json(result))
+    .catch(err => errors.sendError.InternalError(null, res))
 })
 
-router.delete('/:resource/:id', (req, res, next) => {
+router.delete('/:resource/:id', middleware.validateUser, (req, res, next) => {
   let { resource, id } = req.params;
   let controller = controllers[resource];
-  if (controller == null){
-    return res.json(defaultError)
-  }
+
   controller.delete(id)
-  .then(result => {
-    res.json({
-      confirmation: 'success',
-      result,
-    });
-  })
-  .catch(err => {
-    res.status(400).json({
-      confirmation: 'fail',
-      message: err,
-    })
-  })
+    .then(result => res.json(result))
+    .catch(err => errors.sendError.InternalError(null, res))
 })
 
 module.exports = router;
