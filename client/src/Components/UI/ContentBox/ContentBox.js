dc4c9ca1bfe84613d5ecc35f0aa25e78ec9c25d1
philipwisner <philipwisner@gmail.com>
2018-12-17 14:42:19 -0500

    added styling for content box

--- client/src/Components/UI/ContentBox/ContentBox.js
+++ client/src/Components/UI/ContentBox/ContentBox.js
@@ -7,7 +7,7 @@ import Expand from './expand.js';
 
 
 class ContentBox extends PureComponent{
-  
+
   state = {
     showOverlay: false,
     selectAnimation: false,
@@ -22,7 +22,7 @@ class ContentBox extends PureComponent{
       expanded: !prevState
     })
   }
-  
+
   hoverOnSelect = () => {
     this.setState({showOverlay: true})
   }
@@ -46,25 +46,25 @@ class ContentBox extends PureComponent{
       selectedClassPlus = classes.SelectedPlus;
       selectedClass = classes.Selected;
     };
-    const notifications = (this.props.notifications > 0) ? <div className={classes.Notification} data-testid="content-box-ntf">{this.props.notifications}</div> : null;
+    const notifications = (this.props.notifications > 0) ? <div className={classes.Notification} data-testid="content-box-ntf"><span className={classes.NotificationCount}>{this.props.notifications}</span></div> : null;
     return (
     <Aux>
-      <Link 
+      <Link
         to={selecting ? "#" : this.props.link}
-        className={[classes.Container, selectedClass].join(" ")} 
+        className={[classes.Container, selectedClass].join(" ")}
         style={{height: this.state.expanded ? 150 : 50}}
       >
-        <div 
-          data-testid={`content-box-${this.props.title}`} 
+        <div
+          data-testid={`content-box-${this.props.title}`}
           className={classes.SubContainer}
           onClick={selecting ? this.select : null}
           onMouseEnter={selecting ? this.hoverOnSelect : null}
           onMouseLeave={selecting ? () => {this.setState({showOverlay: false})} : null}
         >
-          {this.state.showOverlay ? 
+          {this.state.showOverlay ?
             <div className={classes.SelectOverlay} data-testid={`overlay-${this.props.title}`}>
               <i className={[classes.Plus, "fas fa-plus", animatedClass, selectedClassPlus].join(" ")}></i>
-            </div> 
+            </div>
           : null }
             <div className={classes.TopBanner}>
               <div className={classes.BannerLeft}>
@@ -72,7 +72,7 @@ class ContentBox extends PureComponent{
                   <Icons image={this.props.image} lock={this.props.locked} roomType={this.props.roomType} listType={this.props.listType}/>
                 </div>
                 <div className={classes.Title} data-testid="">{this.props.title}</div>
-                {notifications} 
+                {notifications}
               </div>
               <div className={classes.Expand} style={{transform: this.state.expanded ? `rotate(180deg)` : `rotate(0)`}}><Expand clickHandler={this.toggleExpand}/></div>
             </div>
@@ -82,8 +82,8 @@ class ContentBox extends PureComponent{
                 <div>{this.props.details.description || ''}</div>
                 {this.props.details.facilitators.length > 0 ? <div>Facilitators: {this.props.details.facilitators.map(facilitator => facilitator)}</div> : null}
                 {this.props.details.entryCode ? <div>Entry Code: {this.props.details.entryCode}</div> : null}
-              </div> 
-            : null} 
+              </div>
+            : null}
           </div>
         </div>
       </Link>
