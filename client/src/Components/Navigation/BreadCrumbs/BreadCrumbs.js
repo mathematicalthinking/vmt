50d2e5655ba6360a6d5e2b97ca96d92c3f1113e9
philipwisner <philipwisner@gmail.com>
2018-12-17 11:45:45 -0500

    styled breadcrumbs

--- client/src/Components/Navigation/BreadCrumbs/BreadCrumbs.js
+++ client/src/Components/Navigation/BreadCrumbs/BreadCrumbs.js
@@ -5,19 +5,22 @@ import { Link } from 'react-router-dom';
 const breadCrumbs = props => {
   let crumbs = props.crumbs.map((crumb, i) => {
     let style = classes.Crumb;
-    if (i === props.crumbs.length - 1) { 
-      style = [classes.Crumb, classes.Active].join(' ')
+    let seperatorStyle = classes.Seperator
+    if (i === props.crumbs.length - 1) {
+      style = [classes.Crumb, classes.Active].join(' ');
+      seperatorStyle = [classes.Seperator, classes.Hidden].join(' ');
     }
     return (
-      <Link  key={i} className={glb.Link} to={crumb.link} style={{zIndex: i}}>
+      <Link  key={i} className={[glb.Link, classes.CrumbContainer].join(' ')} to={crumb.link} style={{zIndex: i}}>
         <div className={style} data-testid="crumb">
           {crumb.title}
         </div>
+        <div className={seperatorStyle}> <i className={"fas fa-caret-right"}></i> </div>
       </Link>
     )
   })
   return (
-    <div className={classes.CrumbContainer}>
+    <div className={classes.BreadcrumbContainer}>
       {crumbs}
     </div>
   )
