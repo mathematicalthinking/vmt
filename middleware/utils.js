168430c6cf134b7cee5098e49b501907d00ed6a0
Daniel Kelly <djk8552@gmail.com>
2018-12-17 13:17:23 -0500

    Utility functions for api requests

--- middleware/utils.js
+++ middleware/utils.js
@@ -1,4 +1,17 @@
 const _ = require('lodash');
+const models = require('../models');
+
+const resourceToModelMap = {
+  activities: 'Activity',
+  courses: 'Course',
+  events: 'Event',
+  images: 'Image',
+  messages: 'Message',
+  rooms: 'Room',
+  tabs: 'Tab',
+  teams: 'Team',
+  user: 'User'
+};
 
 const getUser = (req) => {
   return _.propertyOf(req)('user');
@@ -17,7 +30,41 @@ const isValidMongoId = (value) => {
   return regex.test(value);
 };
 
+const getMethod = (req) => {
+  return _.propertyOf(req)('method');
+}
+
+const isModifyRequest = (req) => {
+  let modifyMethods = {
+    POST: true,
+    PUT: true,
+  };
+
+  return modifyMethods[getMethod(req)] === true;
+};
+
+const getModel = (resource) => {
+  return models[resourceToModelMap[resource]];
+};
+
+const getSchema = (resource) => {
+  let model = getModel(resource);
+
+  return _.propertyOf(model)('schema');
+};
+
+const schemaHasProperty = (schema, property) => {
+  if (!_.isString(property)) {
+    return false;
+  }
+  return _.has(schema, `paths.${property}`);
+};
+
 module.exports.getUser = getUser;
 module.exports.getResource = getResource;
 module.exports.getParamsId = getParamsId;
 module.exports.isValidMongoId = isValidMongoId;
+module.exports.isModifyRequest = isModifyRequest;
+module.exports.schemaHasProperty = schemaHasProperty;
+module.exports.getSchema = getSchema;
+module.exports.getModel = getModel;
