153bc05894cbd522216028f5a1f908d2c05ad0a6
Daniel Kelly <djk8552@gmail.com>
2018-12-20 15:07:51 -0500

    Helper functions for determining user role in record that has members array

--- middleware/utils/helpers.js
+++ middleware/utils/helpers.js
@@ -4,4 +4,26 @@ const isNonEmptyObject = (val) => {
   return _.isObject(val) && !_.isEmpty(val);
 };
 
+
+const getUserRoleInRecord = (record, userId) => {
+  let members = _.propertyOf(record)('members');
+
+  if (!_.isArray(members)) {
+    return;
+  }
+
+  let userMemberObject = _.find(members, (obj) => {
+    return _.isEqual(userId, obj.user);
+  });
+
+  return _.propertyOf(userMemberObject)('role');
+
+};
+
+const isUserFacilitatorInRecord = (record, userId) => {
+  return getUserRoleInRecord(record, userId) === 'facilitator';
+}
+
 module.exports.isNonEmptyObject = isNonEmptyObject;
+module.exports.getUserRoleInRecord = getUserRoleInRecord;
+module.exports.isUserFacilitatorInRecord = isUserFacilitatorInRecord
