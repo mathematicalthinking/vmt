fb3d190676302405e2b93bcb728725fc0cd5391f
philipwisner <philipwisner@gmail.com>
2018-12-21 15:05:55 -0500

    made current member collapsable

--- client/src/Components/CurrentMembers/CurrentMembers.js
+++ client/src/Components/CurrentMembers/CurrentMembers.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 import classes from './currentMembers.css';
 import Avatar from '../UI/Avatar/Avatar';
 class CurrentMembers extends Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true
+    }
+  }
+   toggleCollapse() {
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  }
 
   render() {
     const { members, activeMember } = this.props;
     return (
       <div className={classes.Container}>
-        <div className={classes.Title}>
+        <div className={classes.Title} onClick={this.toggleCollapse.bind(this)}>
           Current Members
           <div className={classes.Count}>
             {members.length}
           </div>
         </div>
-        <div className={classes.Left} data-testid='current-members'>
+        <div className={(this.state.expanded ? classes.Expanded : classes.Collapsed)} data-testid='current-members'>
           {members ? members.map(user =>
             <div
               className={[classes.Avatar, (user._id === activeMember ? classes.Active : classes.Passive)].join(" ")}
