47aa581d9da8c18452bbb3201c3caa9f6d136d26
philipwisner <philipwisner@gmail.com>
2018-12-21 16:59:28 -0500

    restructured ws and added room info

--- client/src/Layout/Workspace/Workspace.js
+++ client/src/Layout/Workspace/Workspace.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './workspace.css';
 import { withRouter } from 'react-router-dom';
-import { CurrentMembers, Button, ReplayerControls, EditableText }from '../../Components';
+import { CurrentMembers, Button, ReplayerControls, EditableText, RoomInfo }from '../../Components';
 import {
   GgbGraph,
   GgbActivityGraph,
@@ -146,38 +146,16 @@ const workspaceLayout = React.memo(({
   return (
     <div className={classes.PageContainer}>
       <div className={classes.Container}>
-        <div className={classes.WorkspaceTabs}>
-          {tabs}
-          {role === 'facilitator' ? <div className={[classes.Tab, classes.NewTab].join(' ')}><div onClick={createNewTab}  className={classes.TabBox}><i className="fas fa-plus"></i></div></div> : null}
-        </div>
-        <div className={classes.Top}>
-          <div className={[classes.Graph, classes.Left, "graph"].join(" ")}>{graph}</div>
-          <div className={classes.Right}>
-            <div className={classes.Chat}>{chat}</div>
-            <div className={classes.Members}>
-             {room.members ? <CurrentMembers members={replayer ? replayer.currentMembers.map(member => member.user) : room.currentMembers.map(member => member.user)} activeMember={activeMember}/> : null}
-            </div>
+        <div className={classes.Left}>
+          <div className={classes.WorkspaceTabs}>
+            {tabs}
+            {role === 'facilitator' ? <div className={[classes.Tab, classes.NewTab].join(' ')}><div onClick={createNewTab}    className={classes.TabBox}><i className="fas fa-plus"></i></div></div> : null}
           </div>
-        </div>
-        <div className={classes.Bottom}>
-          <div className={classes.Left}>
+          <div className={[classes.Graph, "graph"].join(" ")}>{graph}</div>
+          <div className={classes.Bottom}>
           {replayer
             ? <ReplayerControls {...replayer} />
-            : <div className={classes.RoomDescription}>
-                {/* <div className={classes.Expand}>Expand</div> */}
-                <h3 className={classes.TabNameTitle}>
-                  <EditableText owner={role === 'facilitator'} inputType={'INPUT'} resource='tab' parentResource={updatedActivity ? 'activity' : 'room'} id={room.tabs[currentTab]._id}  parentId={room._id} field='name'>
-                    {room.tabs[currentTab].name}
-                  </EditableText>
-                </h3>
-                <div className={classes.InstructionsContainer}>
-                  <h4 className={classes.InstructionsTitle}>Instructions: </h4>
-                  <EditableText owner={role === 'facilitator'} inputType={'TEXT_AREA'} resource='tab' parentResource={updatedActivity ? 'activity' : 'room'} id={room.tabs[currentTab]._id} parentId={room._id} field='instructions'>
-                    {room.tabs[currentTab].instructions || room.instructions}
-                  </EditableText>
-                </div>
-                {/* {bottomButton} */}
-              </div>
+            : <RoomInfo role={role} updatedActivity={updatedActivity} room={room} currentTab={currentTab}/>
           }
           {temp && !saved ?
             <div>
@@ -185,36 +163,112 @@ const workspaceLayout = React.memo(({
             </div> : null
           }
           </div>
-          <div className={classes.Right}>
-            <div className={classes.ReferenceWindow}>
-              {!replayer ?
-                <div className={classes.ReferenceControls} onClick={referencing ? clearReference : startNewReference}>
-                  <i className={["fas", "fa-mouse-pointer", classes.MousePointer, referencing ? classes.ReferencingActive : ''].join(" ")}></i>
-                  <div className={classes.ReferenceTool}>Reference</div>
-                  {/* <div className={classes.RefrenceTool}>Perspective</div> */}
-                </div> : null
-              }
-              <div className={classes.LiveLog}>
-
-              </div>
-            </div>
+        </div>
+        <div className={classes.Right}>
+          <div className={classes.Chat}>{chat}</div>
+          <div className={classes.Members}>
+             {room.members ? <CurrentMembers members={replayer ? replayer.currentMembers.map(member => member.user) : room.currentMembers.map(member => member.user)} activeMember={activeMember}/> : null}
+          </div>
+          <div className={classes.ReferenceWindow}>
+            {!replayer ?
+              <div className={classes.ReferenceControls} onClick={referencing ? clearReference : startNewReference}>
+                <i className={["fas", "fa-mouse-pointer", classes.MousePointer, referencing ? classes.ReferencingActive : ''].join(" ")}></i>
+                <div className={classes.ReferenceTool}>Reference</div>
+                {/* <div className={classes.RefrenceTool}>Perspective</div> */}
+              </div> : null
+            }
             <div className={classes.Controls}>
               {!replayer ?
                 <div className={classes.SideButton} onClick={toggleControl}>{controlText}</div> :
                 <div className={classes.SideButton}>Make A Comment</div>
               }
               <div className={[classes.SideButton, classes.Exit].join(" ")} onClick={() => {temp ? history.push('/') : history.goBack()}} theme={'Small'} data-testid='exit-room'>Exit Room</div>
+              {referToCoords && referFromCoords   ?
+                <div className={classes.ReferenceLine}>
+                  <svg height='100%' width='100%' style={{zIndex: 1}}>
+                    <line style={{zIndex: 1500}} x1={referToCoords.left} y1={referToCoords.top} x2={referFromCoords.left} y2={referFromCoords.top} stroke="#2D91F2" strokeWidth="3"/>
+                  </svg>
+                </div>: null
+              }
             </div>
           </div>
         </div>
-        {referToCoords && referFromCoords   ?
-        <div className={classes.ReferenceLine}>
-          <svg height='100%' width='100%' style={{zIndex: 1}}>
-            <line style={{zIndex: 1500}} x1={referToCoords.left} y1={referToCoords.top} x2={referFromCoords.left} y2={referFromCoords.top} stroke="#2D91F2" strokeWidth="3"/>
-          </svg>
-        </div>: null}
       </div>
     </div>
   )
 })
+//   return (
+//     <div className={classes.PageContainer}>
+//       <div className={classes.Container}>
+//         <div className={classes.WorkspaceTabs}>
+//           {tabs}
+//           {role === 'facilitator' ? <div className={[classes.Tab, classes.NewTab].join(' ')}><div onClick={createNewTab}  className={classes.TabBox}><i className="fas fa-plus"></i></div></div> : null}
+//         </div>
+//         <div className={classes.Top}>
+//           <div className={[classes.Graph, classes.Left, "graph"].join(" ")}>{graph}</div>
+//           <div className={classes.Right}>
+            // <div className={classes.Chat}>{chat}</div>
+            // <div className={classes.Members}>
+            //  {room.members ? <CurrentMembers members={replayer ? replayer.currentMembers.map(member => member.user) : room.currentMembers.map(member => member.user)} activeMember={activeMember}/> : null}
+            // </div>
+//           </div>
+//         </div>
+//         <div className={classes.Bottom}>
+          // <div className={classes.Left}>
+          // {replayer
+          //   ? <ReplayerControls {...replayer} />
+          //   : <div className={classes.RoomDescription}>
+          //       {/* <div className={classes.Expand}>Expand</div> */}
+          //       <h3 className={classes.TabNameTitle}>
+          //         <EditableText owner={role === 'facilitator'} inputType={'INPUT'} resource='tab' parentResource={updatedActivity ? 'activity' : 'room'} id={room.tabs[currentTab]._id}  parentId={room._id} field='name'>
+          //           {room.tabs[currentTab].name}
+          //         </EditableText>
+          //       </h3>
+          //       <div className={classes.InstructionsContainer}>
+          //         <h4 className={classes.InstructionsTitle}>Instructions: </h4>
+          //         <EditableText owner={role === 'facilitator'} inputType={'TEXT_AREA'} resource='tab' parentResource={updatedActivity ? 'activity' : 'room'} id={room.tabs[currentTab]._id} parentId={room._id} field='instructions'>
+          //           {room.tabs[currentTab].instructions || room.instructions}
+          //         </EditableText>
+          //       </div>
+          //       {/* {bottomButton} */}
+          //     </div>
+          // }
+          // {temp && !saved ?
+          //   <div>
+          //     <Button theme={'Small'} data-testid='save-temp' style={{zIndex: 1000}} m={20} click={save}>Save This Workspace</Button>
+          //   </div> : null
+          // }
+          // </div>
+        //   <div className={classes.Right}>
+        //     <div className={classes.ReferenceWindow}>
+        //       {!replayer ?
+        //         <div className={classes.ReferenceControls} onClick={referencing ? clearReference : startNewReference}>
+        //           <i className={["fas", "fa-mouse-pointer", classes.MousePointer, referencing ? classes.ReferencingActive : ''].join(" ")}></i>
+        //           <div className={classes.ReferenceTool}>Reference</div>
+        //           {/* <div className={classes.RefrenceTool}>Perspective</div> */}
+        //         </div> : null
+        //       }
+        //       <div className={classes.LiveLog}>
+
+        //       </div>
+        //     </div>
+        //     <div className={classes.Controls}>
+        //       {!replayer ?
+        //         <div className={classes.SideButton} onClick={toggleControl}>{controlText}</div> :
+        //         <div className={classes.SideButton}>Make A Comment</div>
+        //       }
+        //       <div className={[classes.SideButton, classes.Exit].join(" ")} onClick={() => {temp ? history.push('/') : history.goBack()}} theme={'Small'} data-testid='exit-room'>Exit Room</div>
+        //     </div>
+        //   </div>
+        // </div>
+        // {referToCoords && referFromCoords   ?
+        // <div className={classes.ReferenceLine}>
+        //   <svg height='100%' width='100%' style={{zIndex: 1}}>
+        //     <line style={{zIndex: 1500}} x1={referToCoords.left} y1={referToCoords.top} x2={referFromCoords.left} y2={referFromCoords.top} stroke="#2D91F2" strokeWidth="3"/>
+        //   </svg>
+        // </div>: null}
+//       </div>
+//     </div>
+//   )
+// })
 export default withRouter(workspaceLayout);
