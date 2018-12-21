711b196e25f685a4edbc19f447dd4c9ee318cb53
philipwisner <philipwisner@gmail.com>
2018-12-21 13:39:16 -0500

    changed instructions text

--- client/src/Layout/Workspace/Workspace.js
+++ client/src/Layout/Workspace/Workspace.js
@@ -170,7 +170,7 @@ const workspaceLayout = React.memo(({
                     {room.tabs[currentTab].name}
                   </EditableText>
                 </h3>
-                <div>
+                <div className={classes.InstructionsContainer}>
                   <h4 className={classes.InstructionsTitle}>Instructions: </h4>
                   <EditableText owner={role === 'facilitator'} inputType={'TEXT_AREA'} resource='tab' parentResource={updatedActivity ? 'activity' : 'room'} id={room.tabs[currentTab]._id} parentId={room._id} field='instructions'>
                     {room.tabs[currentTab].instructions || room.instructions}
