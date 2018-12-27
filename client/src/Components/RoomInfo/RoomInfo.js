ec85d701134bcd7e94deb7a1f1991b95b4b490e3
philipwisner <philipwisner@gmail.com>
2018-12-27 10:53:28 -0500

    added toggle icon

--- client/src/Components/RoomInfo/RoomInfo.js
+++ client/src/Components/RoomInfo/RoomInfo.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import classes from './roomInfo.css';
 import { EditableText }from '../../Components';
+import Expand from '..//../Components/UI/ContentBox/expand.js';
+
 class RoomInfo extends Component {
   constructor() {
     super();
@@ -18,11 +20,12 @@ class RoomInfo extends Component {
     const { role, updatedActivity, room, currentTab } = this.props;
     return (
     <div className={classes.RoomDescription}>
-      <h3 className={classes.TabNameTitle} onClick={this.toggleCollapse.bind(this)}>
+      <div className={classes.TabNameTitle} >
         <EditableText owner={role === 'facilitator'} inputType={'INPUT'} resource='tab' parentResource={updatedActivity ? 'activity' : 'room'} id={room.tabs[currentTab]._id}  parentId={room._id} field='name'>
           {room.tabs[currentTab].name}
         </EditableText>
-      </h3>
+        <div onClick={this.toggleCollapse.bind(this)} className={classes.ToggleView} style={{transform: this.state.expanded ? `rotate(0)` : `rotate(90deg)`}}><Expand/></div>
+      </div>
       <div className={(this.state.expanded ? classes.InstructionsContainer : classes.Collapsed)}>
         <h4 className={classes.InstructionsTitle}>Instructions: </h4>
         <EditableText owner={role === 'facilitator'} inputType={'TEXT_AREA'} resource='tab' parentResource={updatedActivity ? 'activity' : 'room'} id={room.tabs[currentTab]._id} parentId={room._id} field='instructions'>
