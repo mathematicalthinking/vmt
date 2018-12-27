4b74452222ce6a92f8ef591c221c0d50adf09bc7
okputadora <okputadora@gmail.com>
2018-12-27 18:04:16 -0500

    same thing with activities

--- controllers/ActivityController.js
+++ controllers/ActivityController.js
@@ -18,28 +18,50 @@ module.exports = {
   },
 
   post: (body) => {
-    return new Promise((resolve, reject) => {
-      if (body.template) {
-
+    console.log("BODY: ", body)
+    return new Promise(async (resolve, reject) => {
+      let existingTabs;
+      if (body.activities) {
+        try {
+          let activities = await db.Activity.find({'_id': {$in: [body.activities]}}).populate('tabs')
+          existingTabs = activities.reduce((acc, activity) => {
+            console.log("A.T: ", activity.tabs)
+            return acc.concat(activity.tabs)
+          }, [])
+          console.log("A: ", activities)
+        }
+        catch(err) {reject(err)}
       }
-      delete body.template;
-      delete body.templatePrivacySetting;
       let createdActivity;
       db.Activity.create(body)
       .then(activity => {
         createdActivity = activity;
-        return db.Tab.create({
-          name: 'Tab 1',
-          activity: activity._id,
-          desmosLink: body.desmosLink,
-          ggbFile: body.ggbFile,
-          tabType: body.roomType,
-        })
+        if (!existingTabs) {
+          return db.Tab.create({
+            name: 'Tab 1',
+            activity: activity._id,
+            desmosLink: body.desmosLink,
+            ggbFile: body.ggbFile,
+            tabType: body.roomType,
+          })
+        } else {
+          console.log("EXISTING TABS: ", existingTabs)
+          return Promise.all(existingTabs.map(tab => {
+            let newTab = {...tab}
+            delete newTab._id;
+            newTab.activity = activity._id;
+            return db.Tab.create(newTab)
+          }))
+        }
       })
       .then(tab => {
-        return db.Activity.findByIdAndUpdate(createdActivity._id, {$addToSet: {tabs: tab._id}}, {new: true}).populate({path: 'tabs'})
+        console.log("TABS: ", tab)
+        return db.Activity.findByIdAndUpdate(createdActivity._id, {
+          $addToSet: {tabs: Array.isArray(tab) ? {$each: tab.map(t => t._id)} : tab._id}
+        }, {new: true}).populate({path: 'tabs'})
       })
       .then(activity => {
+        console.log("ACTIVITY: ", activity)
         resolve(activity)
       })
       .catch(err => {
