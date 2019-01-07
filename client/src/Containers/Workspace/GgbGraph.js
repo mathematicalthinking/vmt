adda7c6a94ff16b86efca66c2157ad17442bd3fe
okputadora <okputadora@gmail.com>
2019-01-07 14:20:18 -0500

    fixed control issue on loss of connection

--- client/src/Containers/Workspace/GgbGraph.js
+++ client/src/Containers/Workspace/GgbGraph.js
@@ -65,16 +65,20 @@ class GgbGraph extends Component {
   }
 
   async componentDidUpdate(prevProps) {
+    if (!this.ggbApplet) return;
     let wasInControl = prevProps.room.controlledBy === this.props.user._id;
     let isInControl = this.props.room.controlledBy === this.props.user._id;
     let isSomeoneElseInControl = this.props.room.controlledBy && !isInControl;
     if (!wasInControl && isInControl) {
+      console.log('im in control now')
       this.setState({switchingControl: true}, () => {
-        this.ggbApplet.setMode(0)
-        // im not really sure what this does we seem to get the same menu whether we specify all these numbers or not...but it was breaking the hamburger menu to do so
-        // this.ggbApplet.showToolBar("0 39 73 62 | 1 501 67 , 5 19 , 72 75 76 | 2 15 45 , 18 65 , 7 37 | 4 3 8 9 , 13 44 , 58 , 47 | 16 51 64 , 70 | 10 34 53 11 , 24  20 22 , 21 23 | 55 56 57 , 12 | 36 46 , 38 49  50 , 71  14  68 | 30 29 54 32 31 33 | 25 17 26 60 52 61 | 40 41 42 , 27 28 35 , 6")
-        this.ggbApplet.showMenuBar(true)
-        this.freezeElements(false)
+        if (this.ggbApplet) {
+          this.ggbApplet.setMode(0)
+          // im not really sure what this does we seem to get the same menu whether we specify all these numbers or not...but it was breaking the hamburger menu to do so
+          // this.ggbApplet.showToolBar("0 39 73 62 | 1 501 67 , 5 19 , 72 75 76 | 2 15 45 , 18 65 , 7 37 | 4 3 8 9 , 13 44 , 58 , 47 | 16 51 64 , 70 | 10 34 53 11 , 24  20 22 , 21 23 | 55 56 57 , 12 | 36 46 , 38 49  50 , 71  14  68 | 30 29 54 32 31 33 | 25 17 26 60 52 61 | 40 41 42 , 27 28 35 , 6")
+          this.ggbApplet.showMenuBar(true)
+          this.freezeElements(false)
+        }
         // setTimeout(() => {
           this.setState({switchingControl: false})
         // }, 0)
@@ -83,6 +87,7 @@ class GgbGraph extends Component {
     }
     else if ((wasInControl && !isInControl )|| isSomeoneElseInControl) {
       this.ggbApplet.showToolBar(false)
+      this.ggbApplet.showMenuBar(false)
       this.ggbApplet.setMode(40)
       this.freezeElements(true)
     }
@@ -201,7 +206,6 @@ class GgbGraph extends Component {
     } else if (startingPoint) {
       this.ggbApplet.setXML(startingPoint)
     } else if (ggbFile) {
-      console.log('setting ggbFile')
       this.ggbApplet.setBase64(ggbFile);
     }
     // attach this listeners to the ggbApplet
@@ -215,6 +219,7 @@ class GgbGraph extends Component {
       let definition = this.ggbApplet.getCommandString(label);
       this.sendEvent(xml, definition, label, "ADD", "added");
     }
+    this.ggbApplet.setUndoPoint()
     this.setState({receivingData: false})
   }
 
@@ -222,6 +227,7 @@ class GgbGraph extends Component {
     if (!this.state.receivingData) {
       this.sendEvent(null, null, label, "REMOVE", "removed")
     }
+    this.ggbApplet.setUndoPoint()
     this.setState({receivingData: false})
   }
 
@@ -234,6 +240,7 @@ class GgbGraph extends Component {
       let xml = this.ggbApplet.getXML(label)
       this.sendEvent(xml, null, label, "UPDATE", "updated")
     }
+    this.ggbApplet.setUndoPoint()
     this.setState({receivingData: false})
     // this.ggbApplet.evalCommand("updateConstruction()")
   }
@@ -255,7 +262,7 @@ class GgbGraph extends Component {
   }
 
   registerListeners() {
-    if (this.ggbApplet.listeners.length === 0) {
+    if (this.ggbApplet.listeners.length > 0) {
       this.ggbApplet.unregisterAddListener(this.addListener);
       this.ggbApplet.unregisterUpdateListener(this.updateListener);
       this.ggbApplet.unregisterRemoveListener(this.eventListener);
@@ -268,6 +275,11 @@ class GgbGraph extends Component {
   }
 
   sendEvent = throttle(async (xml, definition, label, eventType, action) => {
+    if (!this.props.user.connected) {
+      console.log("WE NEED TO UNDO THE LAST EVENT")
+      this.ggbApplet.undo()
+      return;
+    }
     let xmlObj;
     if (xml) xmlObj = await this.parseXML(xml)
     let newData = {
