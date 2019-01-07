643cc8ab5f9d3b613f86125a4a93f9c2dacca7f1
okputadora <okputadora@gmail.com>
2019-01-07 09:30:39 -0500

    redirect to signup when user needs to be logged in

--- client/src/Containers/Workspace/GgbGraph.js
+++ client/src/Containers/Workspace/GgbGraph.js
@@ -71,6 +71,7 @@ class GgbGraph extends Component {
     if (!wasInControl && isInControl) {
       this.setState({switchingControl: true}, () => {
         this.ggbApplet.setMode(0)
+        // im not really sure what this does we seem to get the same menu whether we specify all these numbers or not...but it was breaking the hamburger menu to do so
         // this.ggbApplet.showToolBar("0 39 73 62 | 1 501 67 , 5 19 , 72 75 76 | 2 15 45 , 18 65 , 7 37 | 4 3 8 9 , 13 44 , 58 , 47 | 16 51 64 , 70 | 10 34 53 11 , 24  20 22 , 21 23 | 55 56 57 , 12 | 36 46 , 38 49  50 , 71  14  68 | 30 29 54 32 31 33 | 25 17 26 60 52 61 | 40 41 42 , 27 28 35 , 6")
         this.ggbApplet.showMenuBar(true)
         this.freezeElements(false)
--- client/src/Routes/MyVmt.js
+++ client/src/Routes/MyVmt.js
@@ -28,24 +28,13 @@ class MyVmt extends Component {
             <PrivateRoute exact path={`${path}/facilitator`} authed={this.props.loggedIn} component={FacilitatorInstructions} />
             <PrivateRoute exact path={`${path}/:resource`} authed={this.props.loggedIn} component={MyVMT}/>
             <PrivateRoute exact path = {`${path}/courses/:course_id/:resource`} authed={this.props.loggedIn} redirectPath='/signup' component={Course}/>
-            <PrivateRoute exact path={`${path}/courses/:course_id/activities/:activity_id/:resource`} authed={this.props.loggedIn} component={Activity} />
+            <PrivateRoute exact path={`${path}/courses/:course_id/activities/:activity_id/:resource`} authed={this.props.loggedIn} redirectPath='/signup' component={Activity} />
             <PrivateRoute exact path = {`${path}/courses/:course_id/rooms/:room_id/:resource`} redirectPath="/signup" authed={this.props.loggedIn} component={Room}/>
             <PrivateRoute exact path = {`${path}/rooms/:room_id/:resource`} redirectPath='/signup' authed={this.props.loggedIn} component={Room}/>
-            <PrivateRoute exact path={`${path}/activities/:activity_id/:resource`} authed={this.props.loggedIn} component={Activity} />
+            <PrivateRoute exact path={`${path}/activities/:activity_id/:resource`} authed={this.props.loggedIn} redirectPath='/signup' component={Activity} />
             <PrivateRoute exact path={`${path}/workspace/:room_id/replayer`} authed={this.props.loggedIn} component={Replayer} />
-            <PrivateRoute exact path={`${path}/workspace/:activity_id/activity`} authed={this.props.loggedIn} component={ActivityWorkspace} />
+            <PrivateRoute exact path={`${path}/workspace/:activity_id/activity`} authed={this.props.loggedIn} redirectPath='/signup' component={ActivityWorkspace} />
             <PrivateRoute exact path={`${path}/workspace/:room_id`} authed={this.props.loggedIn} component={Workspace} />
-
-            {/* <Route exact path='/publicList/:resource' component={PublicList}/>
-            <Route exact path='/publicResource/room/:room_id/:resource' component={Room}/>
-            <Route exact path='/publicResource/course/:course_id/:resource' component={Course} />
-            <PrivateRoute exact path='/profile/activities/:activity_id/:resource' authed={this.props.loggedIn} component={Activity} />
-            <PrivateRoute exact path = '/profile/rooms/:room_id/:resource' authed={this.props.loggedIn} component={Room} />
-            <PrivateRoute exact path = '/profile/course/:course_id/room/:room_id/:resource' authed={this.props.loggedIn} component={Room} />
-            <PrivateRoute expact path = '/workspace/:room_id/replayer' authed={this.props.loggedIn} component={Replayer} />
-            <PrivateRoute expact path = '/workspace/:room_id' authed={this.props.loggedIn} component={Workspace} />
-            <Route exact path='/dashboard/course/:course_id/room/:room_id/:resource' authed={this.props.loggedIn} component={Room}/>
-            {/* <PrivateRoute path='/assign' authed={this.props.loggedIn} component={Activities}/> */}
             <Route exact path='/confirmation' component={Confirmation} />}
             <Route path="*" render={() => {
               return (<div>Error</div>)
