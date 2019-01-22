beb0e8f35dd893b6098e8f4832e92127ba8b8d9c
okputadora <okputadora@gmail.com>
2019-01-22 11:09:22 -0500

    log server url

--- client/src/utils/sockets.js
+++ client/src/utils/sockets.js
@@ -2,6 +2,7 @@ import io from 'socket.io-client';
 
 
 const socket = io.connect(process.env.REACT_APP_SERVER_URL);
+console.log("server URL: ", process.env.REACT_APP_SERVER_URL);
 socket.removeAllListeners()
 export default socket;
 
