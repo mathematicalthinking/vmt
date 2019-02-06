49ea019d4f373e93958c89b4153a2a7d5f49c8a5
okputadora <okputadora@gmail.com>
2019-02-06 15:34:56 -0500

    added gifs

--- client/src/Layout/About/About.js
+++ client/src/Layout/About/About.js
@@ -1,11 +1,15 @@
 import React from "react";
 import classes from "./about.css";
 import example1 from "./example1.gif";
+import example2 from "./example2.gif";
 import { Background, Aux, DemoBrowser } from "../../Components";
 const About = () => {
   return (
     <Aux>
-      <div className={classes.BackgroundContainer}>
+      <div
+        className={classes.BackgroundContainer}
+        style={{ height: document.body.scrollHeight }}
+      >
         <div className={classes.Background} />
       </div>
       <div className={classes.Container}>
@@ -17,11 +21,59 @@ const About = () => {
         </div>
         <div className={classes.Content}>
           <DemoBrowser>
+            <img src={example1} height={320} alt="example-1" />
+          </DemoBrowser>
+          <p className={classes.Description}>
+            VMT uses{" "}
+            <a
+              className={classes.Link}
+              href="https://www.geogebra.org/"
+              rel="noopener"
+              target="_blank"
+            >
+              GeoGebra
+            </a>{" "}
+            and{" "}
+            <a
+              className={classes.Link}
+              href="https://www.desmos.com/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Desmos
+            </a>{" "}
+            as the underlying workspace so you instantly have access to millions
+            of activities created by teachers and math enthusiasts around the
+            world.
+          </p>
+        </div>
+        <div className={classes.Content}>
+          <p className={classes.Description}>
+            Teachers can manage their entire curriculum by creating courses and
+            resusable activities...
+          </p>
+          <DemoBrowser>
+            <img className={classes.Image} src={example2} alt="example-2" />
+          </DemoBrowser>
+        </div>
+        <div className={classes.Content}>
+          <DemoBrowser>
             <img src={example1} alt="example-1" />
           </DemoBrowser>
           <p className={classes.Description}>
-            VMT leverages GeoGebra and Desmos
+            ...Or anyone can start collaborating immediately simply by inviting
+            another VMT member to their room
           </p>
+        </div>
+        <div className={classes.Content}>
+          <p className={classes.Description}>
+            All of the work done in a room is replayable, allowing teachers and
+            researchers to gain richer insights into student work and
+            collaborative thought processes.
+          </p>
+          <DemoBrowser>
+            <img src={example1} alt="example-1" />
+          </DemoBrowser>
         </div>
       </div>
     </Aux>
