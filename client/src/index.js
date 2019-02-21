c68c389bf121d99643848a1e8f4413ff2768a4d0
okputadora <okputadora@gmail.com>
2019-02-21 13:37:22 -0500

    test

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-
+console.log("testing new update");
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
