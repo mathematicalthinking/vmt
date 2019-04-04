41558a8db4e4c47eb07e4ba0aa91b687c2ff4695
Daniel Kelly <djk8552@gmail.com>
2019-04-04 10:23:43 -0400

    Factor out room fetch
    Now being initiated by encompass

--- client/src/Containers/Replayer/EncompassReplayer.js
+++ client/src/Containers/Replayer/EncompassReplayer.js
@@ -1,80 +1,29 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import classes from "./EncompassReplayer.css";
-import API from "../../utils/apiRequests";
-
-// import WorkspaceLayout from "../../Layout/Workspace/Workspace";
-// import ReplayerControls from "./ReplayerControls";
-// import DesmosReplayer from "./DesmosReplayer";
-// import GgbReplayer from "./GgbReplayer";
-// import ChatReplayer from "./ChatReplayer";
-
-// import CurrentMembers from "../../Components/CurrentMembers/CurrentMembers";
-// import Loading from "../../Components/Loading/Loading";
-
-// import Tabs from "../Workspace/Tabs";
-// import Tools from "../Workspace/Tools/Tools";
-// // import throttle from "lodash/throttle";
-// import moment from "moment";
 import SharedReplayer from "./SharedReplayer";
 
 class EncompassReplayer extends Component {
   state = {
-    roomId: null
+    room: null
   };
 
   componentDidMount() {
     let currentUrl = window.location.hash;
     let roomId = this.getRoomIdFromUrl(currentUrl);
-    this.setState({ roomId });
+    this.setState({ room: this.getRoomFromWindow(roomId) });
 
-    window.addEventListener("hashchange", this.setRoomId, false);
-    // MAKE API TO GET ROOM
-    // Build a log
-    // this.fetchRoom(this.state.roomId)
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.roomId && prevState.roomId !== this.state.roomId) {
-      this.fetchRoom(this.state.roomId);
-    }
+    window.addEventListener("hashchange", this.setRoom, false);
   }
 
   componentWillUnmount() {
-    window.removeEventListener("hashchange", this.setRoomId);
+    window.removeEventListener("hashchange", this.setRoom);
   }
 
-  setRoomId = event => {
+  setRoom = event => {
     let newUrl = event.newURL;
     let roomId = this.getRoomIdFromUrl(newUrl);
-    this.setState({ roomId });
-  };
-
-  fetchRoom = roomId => {
-    API.getById("rooms", roomId, false, true, true)
-      .then(res => {
-        let room = res.data.result;
-        let allEvents = [];
-        room.tabs.forEach(tab => {
-          allEvents = allEvents.concat(tab.events);
-        });
-        allEvents = allEvents
-          .concat(room.chat)
-          .sort((a, b) => a.timestamp - b.timestamp)
-          .filter((entry, i, arr) => {
-            if (arr[i - 1]) {
-              if (entry.description) {
-                return entry.description !== arr[i - 1].description;
-              } else return true;
-            }
-            return true;
-          });
-        room.log = allEvents;
-        this.setState({ room });
-      })
-      .catch(err => {
-        console.log("error ger room", err);
-      });
+    this.setState({ room: this.getRoomFromWindow(roomId) });
   };
 
   getRoomIdFromUrl = url => {
@@ -92,9 +41,15 @@ class EncompassReplayer extends Component {
     return roomId || null;
   };
 
+  getRoomFromWindow = roomId => {
+    if (!window.vmtRooms || !roomId) {
+      return null;
+    }
+    return window.vmtRooms[roomId] || null;
+  };
+
   render() {
     if (this.state.room) {
-      console.log(this.state.room);
       return (
         <div className={classes.EncompassReplayer}>
           <SharedReplayer room={this.state.room} encompass />
