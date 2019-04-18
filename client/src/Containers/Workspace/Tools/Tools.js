fe620154783f61ea3e8b6ed2b24a7f0c78a782d4
okputadora <okputadora@gmail.com>
2019-04-18 12:55:57 -0400

    reorder tools in workspace

--- client/src/Containers/Workspace/Tools/Tools.js
+++ client/src/Containers/Workspace/Tools/Tools.js
@@ -15,12 +15,40 @@ const Tools = React.memo(props => {
   }
   return (
     <div className={classes.Container}>
-      <h3 className={classes.Title}>Tools</h3>
+      {/* <h3 className={classes.Title}>Tools</h3> */}
       <div className={true ? classes.Expanded : classes.Collapsed}>
-        <div className={classes.ReferenceWindow}>
-          <div className={classes.ReferenceControls}>
-            <Awareness lastEvent={lastEvent} />
+        <div className={classes.Controls}>
+          {!replayer ? (
+            <div
+              className={classes.SideButton}
+              role="button"
+              onClick={props.toggleControl}
+            >
+              {controlText}
+            </div>
+          ) : null}
+          <div
+            className={classes.Exit}
+            role="button"
+            onClick={props.goBack}
+            // theme={"Small"}
+            data-testid="exit-room"
+          >
+            Exit {replayer ? "Replayer" : "Room"}
           </div>
+        </div>
+        {props.save ? (
+          <div className={classes.Save}>
+            <div
+              className={classes.SideButton}
+              role="button"
+              onClick={props.save}
+            >
+              save
+            </div>
+          </div>
+        ) : null}
+        <div className={classes.ReferenceWindow}>
           {!replayer ? (
             <div
               className={classes.ReferenceControls}
@@ -43,36 +71,8 @@ const Tools = React.memo(props => {
             </div>
           ) : null}
         </div>
-        {props.save ? (
-          <div className={classes.Save}>
-            <div
-              className={classes.SideButton}
-              role="button"
-              onClick={props.save}
-            >
-              save
-            </div>
-          </div>
-        ) : null}
-        <div className={classes.Controls}>
-          {!replayer ? (
-            <div
-              className={classes.SideButton}
-              role="button"
-              onClick={props.toggleControl}
-            >
-              {controlText}
-            </div>
-          ) : null}
-          <div
-            className={classes.Exit}
-            role="button"
-            onClick={props.goBack}
-            // theme={"Small"}
-            data-testid="exit-room"
-          >
-            Exit {replayer ? "Replayer" : "Room"}
-          </div>
+        <div>
+          <Awareness lastEvent={lastEvent} />
         </div>
       </div>
     </div>
