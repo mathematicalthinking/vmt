15bd772f76dd5d043972a65e34069bdda86b9564
okputadora <okputadora@gmail.com>
2019-04-23 12:13:53 -0400

    tests: replayer

--- client/src/Containers/Replayer/SharedReplayers.test.js
+++ client/src/Containers/Replayer/SharedReplayers.test.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import SharedReplayer from './SharedReplayer';
 
-describe('component - replayer', () => {
-  it('renders the replayer', () => {
-    const comp = shallow(<SharedReplayer />);
+import room from '../../../__mocks__/testRoom';
+import user from '../../../__mocks__/testUser';
 
-    comp.find('button');
+describe('component - SharedReplayer', () => {
+  const mock = jest.fn();
+  const props = {
+    room,
+    user,
+    match: { params: { roomId: room._id } },
+    populateRoom: mock,
+  };
+
+  it('renders the vmt replayer', () => {
+    shallow(<SharedReplayer {...props} />);
+    expect(mock).toHaveBeenCalled();
+  });
+
+  it('builds the log after populating', () => {
+    const wrapper = shallow(<SharedReplayer {...props} loading={true} />);
+    const instance = wrapper.instance();
+    jest.spyOn(instance, 'buildLog');
+    jest.spyOn(instance, 'componentDidUpdate');
+    wrapper.setProps({ loading: false });
+    expect(instance.componentDidUpdate).toHaveBeenCalled();
+    expect(instance.buildLog).toHaveBeenCalled();
+  });
+
+  it('renders the encompass replayer', () => {
+    const mock = jest.fn();
+    const wrapper = shallow(<SharedReplayer {...props} encompass />);
+    const instance = wrapper.instance();
+    jest.spyOn(instance, 'buildLog');
+    instance.componentDidMount();
+    expect(mock).not.toHaveBeenCalled();
+    expect(instance.buildLog).toHaveBeenCalled();
   });
 });
