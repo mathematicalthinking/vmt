66d0e53ffab6c1c57179c081448565e25d9cc700
okputadora <okputadora@gmail.com>
2019-05-07 11:01:52 -0400

    basic layout for profile page

---
+++ client/src/Layout/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import classes from './profile.css';
+import SidePanel from './Dashboard/SidePanel/SidePanel';
+const ProfileLayout = props => {
+  const { user } = props;
+  return <div className={classes.Container} />;
+};
+
+export default ProfileLayout;
