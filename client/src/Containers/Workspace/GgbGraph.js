e2736f62f13d029fad67d295d8bb96223e51018e
okputadora <okputadora@gmail.com>
2019-06-24 11:48:32 -0400

    disconnect socket when leaving room

--- client/src/Containers/Workspace/GgbGraph.js
+++ client/src/Containers/Workspace/GgbGraph.js
@@ -436,7 +436,6 @@ class GgbGraph extends Component {
     }
     this.registerListeners();
     setFirstTabLoaded();
-    // EMIT Ggb loaded to request current state...this will ensure that if events occured while we were loading ggb we'll still get them
   };
 
   /**
