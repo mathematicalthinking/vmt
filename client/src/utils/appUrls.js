a7ddf6d74932b107f269b35a6bcc4e1cf4a0f58c
Daniel Kelly <djk8552@gmail.com>
2019-06-26 12:47:21 -0400

    Client utils for building app urls

---
+++ client/src/utils/appUrls.js
@@ -0,0 +1,35 @@
+export const getRedirectUrl = () => {
+  const env = process.env.NODE_ENV;
+
+  if (env === 'production') {
+    return process.env.REACT_APP_SERVER_URL_PRODUCTION;
+  }
+  if (env === 'staging') {
+    return process.env.REACT_APP_SERVER_URL_STAGING;
+  }
+
+  return process.env.REACT_APP_SERVER_URL_DEV;
+};
+
+export const getMtSsoUrl = () => {
+  const env = process.env.NODE_ENV;
+
+  if (env === 'production') {
+    return process.env.REACT_APP_MT_LOGIN_URL_PRODUCTION;
+  }
+  if (env === 'staging') {
+    return process.env.REACT_APP_MT_LOGIN_URL_PRODUCTION;
+  }
+  if (env === 'test') {
+    return process.env.REACT_APP_MT_LOGIN_URL_TEST;
+  }
+  return process.env.REACT_APP_MT_LOGIN_URL_DEV;
+};
+
+export const getGoogleUrl = () => {
+  const base = getMtSsoUrl();
+  const redirectUrl = getRedirectUrl();
+  const endpoint = 'auth/oauth/google';
+
+  return `${base}/${endpoint}?redirectURL=${redirectUrl}`;
+};
