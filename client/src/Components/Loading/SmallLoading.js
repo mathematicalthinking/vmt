00ab1b263685e5549ca8b6b518c72cd7917ee676
okputadora <okputadora@gmail.com>
2019-07-08 11:55:08 -0400

    loading icon

--- client/src/Components/Loading/SmallLoading.js
+++ client/src/Components/Loading/SmallLoading.js
@@ -2,7 +2,7 @@ import React from 'react';
 import sineWave from './sine.gif';
 
 const SmallLoading = () => {
-  return <img src={sineWave} alt="Loading..." width={200} />;
+  return <img src={sineWave} alt="Loading..." width={100} height={20} />;
 };
 
 export default SmallLoading;
