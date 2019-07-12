7b8ccf8e34faf6bb5aea0fd96cc8a1e819442e41
Daniel Kelly <djk8552@gmail.com>
2019-07-12 10:26:44 -0400

    ForgotPassword container, layout, and css

---
+++ client/src/Containers/ForgotPassword.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import { forgotPassword, clearError } from '../store/actions';
+import { ForgotPassword } from '../Layout';
+
+const mapStateToProps = store => {
+  return {
+    loggedIn: store.user.loggedIn,
+    errorMessage: store.loading.errorMessage,
+    loading: store.loading.loading,
+    successMessage: store.loading.successMessage,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { forgotPassword, clearError }
+)(ForgotPassword);
