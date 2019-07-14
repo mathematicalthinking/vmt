d092eee9445e5a52ff61e6130d26be5766df561c
okputadora <okputadora@gmail.com>
2019-07-14 12:50:07 -0400

    checkbox accepts elements as children

--- client/src/Containers/Stats/Filters.js
+++ client/src/Containers/Stats/Filters.js
@@ -5,8 +5,8 @@ import Checkbox from '../../Components/Form/Checkbox/Checkbox';
 import Avatar from '../../Components/UI/Avatar/Avatar';
 
 const Filters = ({ data, filters, dispatch }) => {
-  console.log(data.members);
-  console.log(filters);
+  //   console.log(data.members);
+  //   console.log(filters);
   return (
     <div className={classes.Container}>
       Filters
@@ -27,20 +27,25 @@ const Filters = ({ data, filters, dispatch }) => {
         </Checkbox>
       </div>
       <div className={classes.Options}>
-        {data.members.map(m => (
-          <Checkbox
-            dataId={m.user._id}
-            checked={filters.users.indexOf(m.user._id) > -1}
-            change={(_, id) => {
-              console.log(id);
-              console.log(m.user._id);
-              dispatch({ type: 'ADD_REMOVE_USER', user: m.user._id });
-            }}
-            key={m.user._id}
-          >
-            <Avatar username={m.user.username} color={m.color} />
-          </Checkbox>
-        ))}
+        {data.members.map(m => {
+          const { _id } = m.user;
+          console.log(typeof _id);
+          return (
+            <Checkbox
+              dataId={_id}
+              checked={filters.users.indexOf(_id) > -1}
+              id={_id}
+              change={(_, id) => {
+                console.log(id);
+                console.log(_id);
+                dispatch({ type: 'ADD_REMOVE_USER', user: _id });
+              }}
+              key={_id}
+            >
+              <Avatar username={m.user.username} color={m.color} />
+            </Checkbox>
+          );
+        })}
       </div>
     </div>
   );
