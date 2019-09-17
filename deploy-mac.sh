519435196c43619a22e09a3570f01c243aefa457
Daniel Kelly <djk8552@gmail.com>
2019-09-17 12:48:59 -0400

    Update deploy-mac script for new file structure

--- deploy-mac.sh
+++ deploy-mac.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
-echo -e "\e[1;33m Making a production build"
+echo -e "\e[1;33m Making a $2 build"
 echo -e "\e[1;30m"
-if [ -d "client/build" ]; then
-  rm -rf client/build
+if [ -d "./client/build" ]; then
+  rm -rf ./client/build
 fi
+if [ -d "./client/encompassBuild" ]; then
+  rm -rf ./client/encompassBuild
+fi
+cd ./server
 npm run build-$2
-if [ -d "client/encompassBuild" ]; then
-  rm -rf client/encompassBuild
-fi
 npm run build-enc-$2
-if [ -d "$2" ]; then
-  rm -rf $2
+cd ..
+if [ -d "./$2" ]; then
+  rm -rf ./$2
 fi
-mkdir $2
-cp -r bin config controllers middleware models routes constants db_migration services $2
-cp app.js package.json socketInit.js sockets.js .env $2
-cd $2
+mkdir ./$2
+cd ./$2
+mkdir ./server
+cd ..
+echo "made $2 directory"
+cd ./server
+cp -r ./bin ./config ./constants ./controllers ./db_migration ./middleware ./models ./routes ./services ../$2/server
+echo "\e[1;33m server copied to $2"
+cp app.js package.json socketInit.js sockets.js ../$2/server
+cp .env ../$2
+echo "\e[1;33mfiles copied"
+cd ../$2
 mkdir client
+mkdir client/build
+mkdir client/encompassBuild
+
 cp -r ../client/build/ client/build
 cp -r ../client/encompassBuild/ client/encompassBuild
+
 echo -e "\e[1;33m Zipping...\e[0m"
 zip -r VMT-$2.zip .
 echo -e "\e[1;33m Copying zipped directory...\e[0m"
@@ -26,6 +40,9 @@ scp ./VMT-$2.zip "$1"@mathematicalthinking.org:/tmp/
 echo -e "\e[1;31m You're not done yet!\e[0m"
 echo -e "\e[1;35m run the following commands to complete the demployment proces"
 echo -e "\e[1;34m $ \e[0m ssh $1@mathematicalthinking"
-echo -e "\e[1;34m $ \e[0m cd /home/$1"
-echo -e "\e[1;34m $ \e[0m su root"
-echo -e "\e[1;34m $ \e[0m bash deploy-$2-2"
+echo -e "\e[1;34m $ \e[0m cd /web/mathematicalthinking/vmt"
+echo -e "\e[1;34m $ \e[0m rm -rf staging && mkdir staging && cd staging"
+echo -e "\e[1;34m $ \e[0m mv /tmp/VMT-$2.zip ."
+echo -e "\e[1;34m $ \e[0m unzip VMT-$2.zip"
+echo -e "\e[1;34m $ \e[0m cd ./server && npm install --only=production"
+echo -e "\e[1;34m $ \e[0m systemctl restart vmt-$2"
