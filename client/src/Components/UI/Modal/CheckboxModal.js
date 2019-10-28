038b8d95cd7f608123daccf7c58c0d40bccad0be
Daniel Kelly <djk8552@gmail.com>
2019-10-28 12:06:23 -0400

    CheckboxModal component
    Used to display info messages and give the user the option to choose not
    to see that message again

---
+++ client/src/Components/UI/Modal/CheckboxModal.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Modal from './Modal';
+import classes from './modal.css';
+import Button from '../Button/Button';
+import { Checkbox } from '../..';
+
+class CheckboxModal extends Component {
+  onSelect(event) {
+    const { onSelect } = this.props;
+
+    if (onSelect) {
+      onSelect(event);
+    }
+  }
+  render() {
+    const {
+      show,
+      closeModal,
+      infoMessage,
+      isChecked,
+      checkboxDataId,
+    } = this.props;
+
+    return (
+      <Modal show={show} closeModal={closeModal}>
+        <div>{infoMessage}</div>
+        <div className={classes.Row}>
+          <Checkbox
+            change={(event) => {
+              this.onSelect(event);
+            }}
+            checked={isChecked}
+            dataId={checkboxDataId}
+          >
+            Do not show me this message again
+          </Checkbox>
+        </div>
+
+        <div className={classes.Row}>
+          <Button m={10} click={closeModal}>
+            Okay
+          </Button>
+        </div>
+      </Modal>
+    );
+  }
+}
+
+CheckboxModal.defaultProps = {
+  isChecked: false,
+  checkboxDataId: null,
+};
+
+CheckboxModal.propTypes = {
+  show: PropTypes.bool.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  infoMessage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+  isChecked: PropTypes.bool,
+  checkboxDataId: PropTypes.string,
+};
+export default CheckboxModal;
