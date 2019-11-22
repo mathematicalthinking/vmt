31cecbd28780f7ffe63836478b15fd4c2fb33e51
Daniel Kelly <djk8552@gmail.com>
2019-11-22 10:54:36 -0500

    Factor out tinygraph url formatting to utils file

---
+++ client/src/Containers/Create/tinyGraphs.utils.js
@@ -0,0 +1,29 @@
+const imageThemes = [
+  'frogideas',
+  'duskfalling',
+  'sugarsweets',
+  'heatwave',
+  'daisygarden',
+  'seascape',
+  'summerwarmth',
+  'bythepool',
+  'berrypie',
+];
+
+const shapes = {
+  activities: 'isogrids',
+  courses: 'labs/isogrids/hexa16',
+  rooms: 'spaceinvaders',
+};
+
+export default (name, resource) => {
+  const theme = imageThemes[Math.floor(Math.random() * imageThemes.length)];
+
+  // /, %, \ break tinygraphs links
+  // escaping characters did not seem to work
+  const urlName = name.replace(/[/,%,\\]/gm, '_');
+
+  return `http://tinygraphs.com/${
+    shapes[resource]
+  }/${urlName}?theme=${theme}&numcolors=4&size=220&fmt=svg`;
+};
