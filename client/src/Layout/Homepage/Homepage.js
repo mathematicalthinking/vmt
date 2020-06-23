527af3a5682bfd32f2a7eaa9e7509dd0dba080df
esopp <esoppenheim@gmail.com>
2020-06-22 22:54:31 -0400

    update version date

--- client/src/Layout/Homepage/Homepage.js
+++ client/src/Layout/Homepage/Homepage.js
@@ -87,7 +87,7 @@ class Homepage extends PureComponent {
               VMT is currently in Alpha. If you encounter bugs or want to
               suggest new features please email vmt@21pstem.org
             </p>
-            <p>last updated: 06.19.2020</p>
+            <p>last updated: 06.22.2020</p>
           </section>
           <section className={classes.Options} ref={this.containerRef} />
         </div>
