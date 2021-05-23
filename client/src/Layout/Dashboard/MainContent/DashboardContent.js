8cfa1e871d4e6afc2ac6d59e604120443b6df922
exidy80 <exidy80@gmail.com>
2021-05-22 22:09:37 -0400

    Changed myVMT resource name to 'monitor'

--- client/src/Layout/Dashboard/MainContent/DashboardContent.js
+++ client/src/Layout/Dashboard/MainContent/DashboardContent.js
@@ -6,10 +6,10 @@ import ResourceList from './ResourceList';
 
 export default function DashboardContent(props) {
   const { resource } = props;
-  // Resource could be rooms, courses, activities, or monitoring. I'm breaking the consitency here as "Monitoring"
+  // Resource could be rooms, courses, activities, or monitor. I'm breaking the consitency here as "Monitor"
   // isn't a resource, but a different way of viewing rooms. Also, I should NOT have a literal string written here.
   // Instead, we could have the resource types (and how they should appear to the user) be among the constants.
-  return resource === 'monitoring' ? (
+  return resource === 'monitor' ? (
     <MonitoringView {...props} />
   ) : (
     <ResourceList {...props} />
