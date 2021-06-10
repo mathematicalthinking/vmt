4a1d37fee91c4df75bc0be5f7926d3b52f68ddcc
ANZook <azook@21pstem.org>
2021-06-10 14:19:41 -0400

    new user color sequence

--- client/src/utils/colorMap.js
+++ client/src/utils/colorMap.js
@@ -2,24 +2,24 @@ module.exports = {
   // At least WCAG AA Normal Text against #ffffff
   // amber
   0: '#A7230C',
-  // purple
-  1: '#2929FF',
-  // lime
-  2: '#105102',
-  // baby blue
-  3: '#11799C',
+  // sky blue
+  1: '#105BB2',
+  // moss
+  2: '#197B7B',
+  // salmon
+  3: '#BD0023',
   // lilac
   4: '#BF00E0',
-  // moss
-  5: '#148572',
   // gold
-  6: '#9E6400',
-  // sky blue
-  7: '#105BB2',
+  5: '#9E6400',
+  // purple
+  6: '#2929FF',
   // peach
-  8: '#EB1400',
-  // salmon
-  9: '#BD0023',
+  7: '#EB1400',
+  // baby blue
+  8: '#11799C',
+  // lime
+  9: '#105102',
   //  magenta
-  10: '#B92257',
+  10: '#750044',
 };
