85c2db8467bffe71076fff363eca068a04eb2cf1
ANZook <azook@21pstem.org>
2021-07-21 08:26:19 -0400

    cleaned logs and updated TS

--- client/src/Containers/Workspace/Workspace.js
+++ client/src/Containers/Workspace/Workspace.js
@@ -325,7 +325,6 @@ class Workspace extends Component {
             // eslint-disable-next-line no-console
             console.log(err); // HOW SHOULD WE HANDLE THIS
           }
-          console.log('Current members: ', room.currentMembers);
           this.setState({
             currentMembers: room.currentMembers,
           });
