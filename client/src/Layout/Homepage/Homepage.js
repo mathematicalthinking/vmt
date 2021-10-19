820bed3bd3d4e305e1ba01488a4994e68facd9e6
anzook <azook@21pstem.org>
2021-10-19 09:01:25 -0400

    TS Update - 10/19:v.1.3.12

--- client/src/Layout/Homepage/Homepage.js
+++ client/src/Layout/Homepage/Homepage.js
@@ -96,7 +96,7 @@ class Homepage extends PureComponent {
     const { history } = this.props;
     const { error, pageLocation } = this.state;
     // hoisting for easy access to update @TIMESTAMP and for @todo later streamlining
-    const dateStamp = <p>Last updated: 10.14.2021, v.1.3.11</p>;
+    const dateStamp = <p>Last updated: 10.19.2021, v.1.3.12</p>;
     const maintWindow =
       process.env.REACT_APP_VMT_PROD_MAINT_SCHEDULE || 'Sunday, 3-7pm EST';
     return (
