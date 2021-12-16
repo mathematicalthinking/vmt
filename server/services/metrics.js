8581bd93834ccac0ce0237c75f2312dff5e5adab
anzook <azook@21pstem.org>
2021-12-16 16:34:29 -0500

    Updated cherry-picked changes

--- server/services/metrics.js
+++ server/services/metrics.js
@@ -1,30 +1,38 @@
-// const axios = require('axios');
 const express = require('express');
 
 const router = express.Router();
 const client = require('prom-client');
+const socketInit = require('../socketInit');
 
-// const errors = require('../middleware/errors');
+// Create a Registry which registers the metrics
+const register = new client.Registry();
 
-const collectDefaultMetrics = client.collectDefaultMetrics;
-const Registry = client.Registry;
-const register = new Registry();
-collectDefaultMetrics({ register });
+// Add a default label which is added to all metrics
+register.setDefaultLabels({
+  app: 'VMT_' + process.env.NODE_ENV + '_',
+});
+// const prefix = 'VMT_' + process.env.NODE_ENV + '_';
+
+// Enable the collection of default metrics
+client.collectDefaultMetrics({ register });
+
+const sockets = socketInit;
+const totalSocketconnections = new client.Gauge({
+  name: 'socket_connections',
+  help: 'current socket connections',
+  collect() {
+    // Invoked when the registry collects its metrics' values.
+    // This can be synchronous or it can return a promise/be an async function.
+    if (sockets.io) this.set(sockets.io.engine.clientsCount);
+  },
+});
+
+// Register the histogram
+register.registerMetric(totalSocketconnections);
 
 router.get('/', async (req, res) => {
-  //   axios({
-  //     method: 'GET',
-  //     // headers: { Accept: 'application/json' },
-  //   })
-  //     .then((res) => {
-  // Return all metrics the Prometheus exposition format
   res.set('Content-Type', register.contentType);
   res.end(await register.metrics());
-  // })
-  // .catch((err) => {
-  //   console.log(err);
-  //   return errors.sendError.InternalError(null, res);
-  // });
 });
 
 module.exports = router;
