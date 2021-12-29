3def6921e0be1a58b45d2d39c2cd6a917da6941e
anzook <azook@21pstem.org>
2021-12-29 17:14:16 -0500

    room data provider refetchInterval fix

--- client/src/Containers/Room.js
+++ client/src/Containers/Room.js
@@ -6,7 +6,7 @@ export default function Room(props) {
   const { match } = props;
   const { room_id } = match.params;
   const { isSuccess, data } = usePopulatedRoom(room_id, false, {
-    refresh: 10000,
+    refetchInterval: 10000,
   });
 
   return <RoomLobby {...props} room={isSuccess ? data : undefined} />;
