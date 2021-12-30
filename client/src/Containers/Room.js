38a9992ae3f280129e229be9933174e4376a69da
anzook <azook@21pstem.org>
2021-12-29 22:02:07 -0500

    Room lobby prop spread from store and db

--- client/src/Containers/Room.js
+++ client/src/Containers/Room.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { usePopulatedRoom } from 'utils';
 import RoomLobby from './RoomLobby';
 
-export default function Room(props) {
-  const { match } = props;
+function Room(props) {
+  const { match, room } = props;
   const { room_id } = match.params;
   const { isSuccess, data } = usePopulatedRoom(room_id, false, {
     refetchInterval: 10000,
   });
+  let updatedRoom = {};
+  updatedRoom = isSuccess ? { ...room, ...data } : undefined;
 
-  return <RoomLobby {...props} room={isSuccess ? data : undefined} />;
+  return <RoomLobby {...props} room={updatedRoom} />;
 }
+
+Room.propTypes = {
+  room: PropTypes.shape({}),
+  history: PropTypes.shape({}).isRequired,
+  match: PropTypes.shape({}).isRequired,
+};
+
+Room.defaultProps = {
+  room: null,
+};
+
+const mapStateToProps = (state, ownProps) => {
+  // eslint-disable-next-line camelcase
+  const { room_id } = ownProps.match.params;
+  return {
+    room: state.rooms.byId[room_id],
+  };
+};
+
+export default connect(mapStateToProps)(Room);
