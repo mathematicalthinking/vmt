ad88c7a33d2ad61897b923ec0a32148435729e12
Irv Katz <60225276+exidy80@users.noreply.github.com>
2022-01-06 15:53:26 -0500

    updated to separate two types of assertions

--- server/cypress/integration/createRoom.js
+++ server/cypress/integration/createRoom.js
@@ -4,17 +4,17 @@ const room = require('../fixtures/room');
 const users = require('../../seeders/users');
 
 describe('shows corrects info after creating resources', function() {
-  before(function() {
+  beforeEach(function() {
     cy.task('restoreAll').then(() => {
       cy.login(user);
     });
   });
 
-  after(function() {
-    // cy.logout();
+  afterEach(function() {
+    cy.logout();
   });
 
-  it('creates a new public ggb room & lists it in user resources', function() {
+  it('creates a new public ggb room & lists it once in user resources', function() {
     const { name } = room;
 
     cy.getTestElement('tab')
@@ -43,6 +43,40 @@ describe('shows corrects info after creating resources', function() {
     cy.wait(1000);
 
     cy.getTestElement(`content-box-${name}`).should('have.length', 1);
+
+    const originalRooms = users[0].rooms.length;
+    cy.getTestElement('box-list')
+      .children()
+      .should('have.length', originalRooms + 1);
+  });
+
+  it('creates a new public ggb room & exactly ONE new resources is added to list', function() {
+    const { name } = room;
+
+    cy.getTestElement('tab')
+      .contains('Rooms')
+      .click();
+
+    cy.getTestElement(`create-room`).click();
+
+    cy.get(`input[name=name]`).type(name);
+    cy.get('button')
+      .contains('create')
+      .click();
+    cy.get('button')
+      .contains('next')
+      .click();
+    cy.get('button')
+      .contains('next')
+      .click();
+    cy.get('button')
+      .contains('next')
+      .click();
+    cy.get('button')
+      .contains('create')
+      .click();
+
+    cy.wait(1000);
 
     const originalRooms = users[0].rooms.length;
     cy.getTestElement('box-list')
