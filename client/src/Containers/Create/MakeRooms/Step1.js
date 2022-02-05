391388ad15a2145b72948ab78db6607bbb59147c
Irv Katz <60225276+exidy80@users.noreply.github.com>
2022-02-04 20:39:44 -0500

    :tada: first draft new UI for standalone templates

--- client/src/Containers/Create/MakeRooms/Step1.js
+++ client/src/Containers/Create/MakeRooms/Step1.js
@@ -2,124 +2,124 @@
 /* eslint-disable react/no-did-update-set-state */
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import classes from './makeRooms.css';
+import SearchResults from 'Containers/Members/SearchResults';
+import API from 'utils/apiRequests';
+import { Button, InfoBox, Search, Member } from 'Components';
 import DueDate from '../DueDate';
-import { Button, Search } from '../../../Components';
-import ParticipantList from './ParticipantList';
-import API from '../../../utils/apiRequests';
+import classes from './makeRooms.css';
 
 class Step1 extends Component {
   state = {
     searchResults: [],
-    nominatedParticipants: [], // nominated as opposed to selectedParticipants (which we get from props) when we
+    searchText: '',
+    currentParticipants: [],
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    const { selectedParticipants } = this.props;
-    const { searchResults } = prevState;
-    // Add users to the selected list so we can persist them after we start a new a search
-    // Perhpas this would be a good time to deriveStateFromProps since...that's what we're doing
-    // this seems bad actualy...we're duplicating parent state here...we ashould just rename this local state
-    // parent state = paritcipants we're confirmed are being adding to the room. this component's state = staging
-    if (prevProps.selectedParticipants.length < selectedParticipants.length) {
-      const selectedUser = searchResults.filter((user) =>
-        selectedParticipants.find((userId) => user._id.toString() === userId)
-      );
-      this.setState((previousState) => ({
-        nominatedParticipants: previousState.nominatedParticipants.concat(
-          selectedUser
-        ),
-      }));
-      // Remove users who have been de-selected
-    } else if (
-      prevProps.selectedParticipants.length > selectedParticipants.length
-    ) {
-      this.setState((previousState) => ({
-        nominatedParticipants: previousState.nominatedParticipants.filter(
-          (user) => {
-            return selectedParticipants.find((userId) => user._id === userId);
-          }
-        ),
-      }));
-    }
-  }
+  // componentDidUpdate(prevProps, prevState) {
+  //   const { selectedParticipants } = this.props;
+  //   const { searchResults } = prevState;
+  //   // Add users to the selected list so we can persist them after we start a new a search
+  //   // Perhpas this would be a good time to deriveStateFromProps since...that's what we're doing
+  //   // this seems bad actualy...we're duplicating parent state here...we ashould just rename this local state
+  //   // parent state = paritcipants we're confirmed are being adding to the room. this component's state = staging
+  //   if (prevProps.selectedParticipants.length < selectedParticipants.length) {
+  //     const selectedUser = searchResults.filter((user) =>
+  //       selectedParticipants.find((userId) => user._id.toString() === userId)
+  //     );
+  //     this.setState((previousState) => ({
+  //       nominatedParticipants: previousState.nominatedParticipants.concat(
+  //         selectedUser
+  //       ),
+  //     }));
+  //     // Remove users who have been de-selected
+  //   } else if (
+  //     prevProps.selectedParticipants.length > selectedParticipants.length
+  //   ) {
+  //     this.setState((previousState) => ({
+  //       nominatedParticipants: previousState.nominatedParticipants.filter(
+  //         (user) => {
+  //           return selectedParticipants.find((userId) => user._id === userId);
+  //         }
+  //       ),
+  //     }));
+  //   }
+  // }
 
   search = (text) => {
-    const { selectedParticipants, userId } = this.props;
+    const { userId } = this.props;
+    const { currentParticipants } = this.state;
     if (text.length > 0) {
       API.search(
         'user',
         text,
-        [userId].concat(selectedParticipants.map((p) => p.user._id)) // Exclude myself and already selected members from th search
+        [userId].concat(currentParticipants.map((p) => p._id)) // Exclude myself and already selected members from th search
       )
         .then((res) => {
           const searchResults = res.data.results.filter(
             (user) => user.accountType !== 'temp'
           );
-          this.setState({ searchResults });
+          this.setState({ searchResults, searchText: text });
         })
         .catch((err) => {
           console.log('err: ', err);
         });
     } else {
-      this.setState({ searchResults: [] });
+      this.setState({ searchResults: [], searchText: text });
     }
   };
+
+  addParticipant = (_id, username) => {
+    this.setState((prevState) => ({
+      searchResults: prevState.searchResults.filter((user) => user._id !== _id),
+      currentParticipants: [
+        ...prevState.currentParticipants,
+        { _id, username },
+      ],
+    }));
+  };
+
   render() {
-    const {
-      dueDate,
-      setDueDate,
-      nextStep,
-      course,
-      selectedParticipants,
-      select,
-    } = this.props;
-    const { nominatedParticipants, searchResults } = this.state;
-    const uniqueIds = [];
-    const list = nominatedParticipants
-      .map((member) => ({ user: member }))
-      .concat(searchResults.map((member) => ({ user: member })))
-      .filter((member) => {
-        if (uniqueIds.find((id) => id === member.user._id)) {
-          return false;
-        }
-        uniqueIds.push(member.user._id);
-        return true;
-      });
+    const { dueDate, setDueDate, nextStep, course } = this.props;
+    const { currentParticipants, searchText, searchResults } = this.state;
 
     return (
       <div className={classes.Container}>
         <DueDate dueDate={dueDate} selectDate={setDueDate} />
         {!course && (
-          <Fragment>
-            <h2 className={classes.Title}>Selected Participants</h2>
-            <ParticipantList
-              list={selectedParticipants}
-              selectedParticipants={selectedParticipants}
-              select={select}
-            />
-          </Fragment>
-        )}
-        {!course && (
-          <Fragment>
-            <h2 className={classes.Title}>Add Participants</h2>
-            <div className={classes.SubContainer}>
+          <InfoBox
+            title="Add Participants"
+            icon={<i className="fas fa-user-plus" />}
+          >
+            <Fragment>
               <Search
-                theme="Dark"
                 data-testid="member-search"
                 _search={this.search}
-                placeholder="search by username or email address"
+                placeholder="search existing VMT users by username or email address"
               />
-            </div>
-            <div className={classes.ParticipantList}>
-              <ParticipantList
-                list={list}
-                selectedParticipants={selectedParticipants}
-                select={select}
-              />
-            </div>
-          </Fragment>
+              {searchResults.length > 0 ? (
+                <SearchResults
+                  searchText={searchText}
+                  usersSearched={searchResults}
+                  inviteMember={this.addParticipant}
+                />
+              ) : null}
+            </Fragment>
+          </InfoBox>
         )}
+        <InfoBox title="Participants" icon={<i className="fas fa-users" />}>
+          <div
+            data-testid="members"
+            style={{ overflowY: 'scroll', maxHeight: '300px' }}
+          >
+            {currentParticipants.map((user) => (
+              <Member
+                info={{ user: { _id: user._id, username: user.username } }}
+                key={user._id}
+                resourceName="template"
+              />
+            ))}
+          </div>
+        </InfoBox>
         <div className={classes.ModalButton}>
           <Button m={5} click={nextStep} data-testid="next-step-assign">
             Next
@@ -135,8 +135,6 @@ Step1.propTypes = {
   setDueDate: PropTypes.func.isRequired,
   nextStep: PropTypes.func.isRequired,
   userId: PropTypes.string.isRequired,
-  selectedParticipants: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-  select: PropTypes.func.isRequired,
   course: PropTypes.string,
 };
 
