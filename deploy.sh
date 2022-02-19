0f7322d03fcd44247e94a1ab21929a93d7f0789b
Irv Katz <exidy80@users.noreply.github.com>
2022-02-18 20:06:13 -0800

    deploy script zips more convenient structure

--- deploy.sh
+++ deploy.sh
@@ -16,16 +16,18 @@ if [ -d "./$1" ]; then
 fi
 mkdir ./$1
 cd ./$1
+mkdir vmt
+cd vmt
 mkdir ./server
-cd ..
+cd ../..
 echo "made $1 directory"
 cd ./server
-cp -r ./bin ./config ./constants ./controllers ./db_migration ./middleware ./models ./routes ./services ../$1/server
+cp -r ./bin ./config ./constants ./controllers ./db_migration ./middleware ./models ./routes ./services ../$1/vmt/server
 echo "\e[1;33m server copied to $1"
-cp app.js package.json socketInit.js sockets.js ../$1/server
-cp .env ../$1
+cp app.js package.json socketInit.js sockets.js ../$1/vmt/server
+cp .env ../$1/vmt
 echo "\e[1;33mfiles copied"
-cd ../$1
+cd ../$1/vmt
 mkdir client
 mkdir client/build
 mkdir client/encompassBuild
@@ -33,6 +35,7 @@ mkdir client/encompassBuild
 cp -r ../client/build client/
 cp -r ../client/encompassBuild client/
 
+cd ..
 echo -e "\e[1;33m Zipping...\e[0m"
 zip -r VMT-$1.zip .
 mv VMT-$1.zip ..
