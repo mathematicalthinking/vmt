22c42123397c9260e04002f4d23e267dbbc361e7
John-Witter <mrsolginski@gmail.com>
2022-05-06 11:18:55 -0700

    presumptiveEmailAddress vs selectedEmailAddress

--- client/src/Components/HOC/OauthReturn.js
+++ client/src/Components/HOC/OauthReturn.js
@@ -4,11 +4,32 @@ import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getUser } from '../../store/actions';
 import SmallLoading from '../Loading/SmallLoading';
+import { Button, Modal } from '../../Components';
+import GoogleLogin from '../../Components/Form/Google/LoginButton';
 
 class OauthReturn extends Component {
   componentDidMount() {
-    const { connectGetUser } = this.props;
-    connectGetUser();
+    const {
+      connectGetUser,
+      presumptiveEmailAddress,
+      selectedEmailAddress,
+    } = this.props;
+    if (presumptiveEmailAddress !== selectedEmailAddress) {
+      // If the gmail acct that the user selected on the Google Oauth page
+      // differs from the email address that was used for them during import,
+      // ask the user what email they want to use.
+      // If they want to use the acct that they selected, continue as normal,
+      // else redirect back to the Google Oauth page.
+      <Modal>
+        <div>
+          You selected a different email address than the one used to import you with into VMT. Is this okay?
+        <Button click={connectGetUser()}>
+          Yup
+        </Button>
+        Nope <GoogleLogin />
+        </div>
+      </Modal>
+    } else connectGetUser();
   }
 
   render() {
@@ -31,18 +52,22 @@ OauthReturn.propTypes = {
   connectGetUser: PropTypes.func.isRequired,
   errorMessage: PropTypes.string,
   loading: PropTypes.bool,
+  presumptiveEmailAddress: PropTypes.string,
 };
 
 OauthReturn.defaultProps = {
   loggedIn: false,
   errorMessage: null,
   loading: false,
+  presumptiveEmailAddress: '',
 };
 const mapStateToProps = (state) => {
   return {
     loggedIn: state.user.loggedIn,
     errorMessage: state.loading.errorMessage,
     loading: state.loading.loading,
+    presumptiveEmailAddress: state.user.presumptiveEmailAddress,
+    selectedEmailAddress: state.user.email,
   };
 };
 
