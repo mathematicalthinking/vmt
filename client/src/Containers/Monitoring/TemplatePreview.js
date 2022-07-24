166152875e3ea614ad00b279bb8ee1e7c33184a0
Irv Katz <60225276+exidy80@users.noreply.github.com>
2022-07-24 13:41:08 -0400

    Template now sorts rooms. Fix assign tab for standalone templates.

--- client/src/Containers/Monitoring/TemplatePreview.js
+++ client/src/Containers/Monitoring/TemplatePreview.js
@@ -6,24 +6,10 @@ import classes from './monitoringView.css';
 import RoomsMonitor from './RoomsMonitor';
 
 /**
- * The TemplatePreview provides three views into a set of rooms: activity graph, thumbnail, and chat. Users can
- * select which of all the rooms they manage (from rooms). The set of rooms are the rooms that were instantiated
- * from the current template (given by the activityId prop -- and maybe some other useful props TBD).
- *
- * The component is similar to MonitoringView in that the views of the rooms are laid out as tiles. Additionally,
- * hovering on the three dots next to a title brings up a menu: Enter Room, Manage Members, Open Replayer, and View Room Stats.
- * However, differences vs. MonitoringView are:
- * - The tiles have their room NUMBER at top
- * - there is an overall dropdown for selecting which tab and / or screen to show on ALL the room tiles. (@TODO How will
- *      we determine which screen numbers to show? Somehow get maximum number of screens or just show only screens for
- *      which at least one room has a template?)
- * - there is no sense of "selecting" which rooms to preview. ALL rooms for the current template are shown.
- * on the notification icon brings up the notifications in a modal window for that room (@TODO).
- *
- * @TODO Decide whether TemplatePreview should connect to the Redux store to maintain the state of the preview, such as the size
- * of room tiles, which type of preview is selected (thumbail, stat), which screen or tab of the template is being shown.
- *
- *
+ * The TemplatePreview provides an overview of the rooms that were created from a template.  This view is
+ * similar to CourseMonitor except that there is an overall dropdown for selecting which tab and / or screen to show on ALL the room tiles. (@TODO How will
+ * we determine which screen numbers to show? Somehow get maximum number of screens or just show only screens for
+ * which at least one room has a template?)
  */
 
 function TemplatePreview({ activity }) {
@@ -35,17 +21,10 @@ function TemplatePreview({ activity }) {
   // for the API-retrieved data) at the top level rather than inside
   // of a useEffect.
   const queryStates = {};
-  activity.rooms.forEach((roomId) => {
-    queryStates[roomId] = usePopulatedRoom(
-      roomId,
-      true,
-      // Check for updates every 10 sec. If we are viewing rooms (i.e., Chat, Thumbnail, or Graph), then we need
-      // to update only the currently selected rooms. If we are selecting rooms via the selection table, then we
-      // should try to update all rooms so that the "current in room" column remains correct.
-      {
-        refetchInterval: 10000, // @TODO Should experiment with longer intervals to see what's acceptable to users (and the server)
-      }
-    );
+  activity.rooms.forEach((room) => {
+    queryStates[room._id] = usePopulatedRoom(room._id, true, {
+      refetchInterval: 10000, // @TODO Should experiment with longer intervals to see what's acceptable to users (and the server)
+    });
   });
 
   return (
@@ -66,11 +45,12 @@ function TemplatePreview({ activity }) {
       )}
       <RoomsMonitor
         populatedRooms={activity.rooms
-          .filter((roomId) => queryStates[roomId].isSuccess)
+          .sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))
+          .filter((room) => queryStates[room._id].isSuccess)
           .reduce(
-            (res, roomId) => ({
+            (res, room) => ({
               ...res,
-              [roomId]: queryStates[roomId].data,
+              [room._id]: queryStates[room._id].data,
             }),
             {}
           )}
