96f1f51400b5482b266d138d8a3cee5b48a6c5b1
Irv Katz <60225276+exidy80@users.noreply.github.com>
2022-08-25 20:21:42 -0400

    fixed grammar in notifications

--- client/src/Components/HOC/socketProvider.utils.js
+++ client/src/Components/HOC/socketProvider.utils.js
@@ -2,31 +2,29 @@ export default (notification, course, room, myResources) => {
   let message = null;
   const type = notification.notificationType;
   const resource = notification.resourceType;
+  const username = notification.fromUser ? notification.fromUser.username : '';
   // @todo refactor switch statements\
   if (type === 'requestAccess') {
-    const { username } = notification.fromUser;
     message = `${username} is requesting to join ${resource} ${
       myResources[`${resource}s`][notification.resourceId]
         ? myResources[`${resource}s`][notification.resourceId].name
         : '<name not found>'
     }`;
   } else if (type === 'grantedAccess') {
-    message = `your request to join ${resource} ${
+    message = `Your request to join ${resource} ${
       resource === 'room' ? room.name : course.name
     } has been accpeted`;
   } else if (type === 'newMember') {
-    const { username } = notification.fromUser;
     message = `${username} joined ${resource} ${
       myResources[`${resource}s`][notification.resourceId]
         ? myResources[`${resource}s`][notification.resourceId].name
         : '<name not found>'
     } `;
   } else if (type === 'invitation') {
-    message = `you have been invited you to join ${resource} ${
+    message = `You have been invited to join ${resource} ${
       resource === 'room' ? room.name : course.name
     }`;
   } else if (type === 'assignedNewRoom') {
-    const { username } = notification.fromUser;
     message = `${username} has assigned you to room ${room.name}`;
   }
   return message;
