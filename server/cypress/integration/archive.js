ef340b4e730b25074032a4d1ef9b3d34e5c08156
John-Witter <mrsolginski@gmail.com>
2023-01-26 12:54:16 -0700

    screen.getBy to cy.findBy

--- server/cypress/integration/archive.js
+++ server/cypress/integration/archive.js
@@ -2,44 +2,44 @@
 
 // 2. click Archive page
 /*
-        screen.getByRole('link', {  name: /archive/i})
+        cy.findByRole('link', {  name: /archive/i})
 */
 
 // 3. Archive page should have 1 room: "room 1"
 /*
-        screen.getByRole('heading', {  name: /search your 1 archived rooms/i})
+        cy.findByRole('heading', {  name: /search your 1 archived rooms/i})
  */
 
 // 4. click unarchive icon
 /*
-        const button = screen.getByRole('button', {  name: /unarchive unarchive output/i});within(button).getByText(/output/i);
+        const button = cy.findByRole('button', {  name: /unarchive unarchive output/i});within(button).getByText(/output/i);
  */
 
 // 5. click yes on popup modal
 /*
-        screen.getByRole('button', {  name: /yes/i})
+        cy.findByRole('button', {  name: /yes/i})
 */
 
 // 6. "room 1" should no longer exist
 
 // 7. click MyVMT in navbar
 /*
-        screen.getByRole('link', {  name: /my vmt/i})
+        cy.findByRole('link', {  name: /my vmt/i})
 */
 
 // 8. click "Last Week"
 /*
-        screen.getByText(/last week/i)
-        */
+        cy.findByText(/last week/i)
+*/
 
 // 9. click "All" in the drop down
 /*
-        screen.getByText(/last week/i)
+        cy.findByText(/last week/i)
 */
 
 // 10. "room 1" should exist
 /*
-        screen.getByText(/room 1/i)
+        cy.findByText(/room 1/i)
 */
 
 // @TODO: server > seeders > tab.js -> ObjectId('5d3b493dca44f53a90a9ed35') is used by 3 rooms and each room should have unique tabs.
