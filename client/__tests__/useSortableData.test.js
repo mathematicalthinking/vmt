e17e1b88dcdf99869880a3793c4e2a0cdffc1c19
Irv Katz <60225276+exidy80@users.noreply.github.com>
2023-07-29 11:02:41 -0400

    :tada: first commit

--- client/__tests__/useSortableData.test.js
+++ client/__tests__/useSortableData.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import { useSortableData, timeFrames } from '../src/utils/useSortableData';
 
 const items = [
@@ -14,21 +14,21 @@ const items = [
     name: 'Jane',
     age: 24,
     email: 'jane@example.com',
-    timestamp: '2022-02-02T12:34:56.789Z',
+    timestamp: '2023-07-02T12:34:56.789Z',
   },
   {
     id: 3,
     name: 'Bob',
     age: 25,
     email: 'bob@example.com',
-    timestamp: '2022-03-03T12:34:56.789Z',
+    timestamp: '2023-07-03T12:34:56.789Z',
   },
 ];
 
 describe('useSortableData', () => {
   it('should sort items by id in ascending order', () => {
     const { result } = renderHook(() => useSortableData(items));
-    result.current.requestSort('id');
+    act(() => result.current.requestSort('id'));
 
     expect(result.current.items).toEqual([
       {
@@ -43,22 +43,26 @@ describe('useSortableData', () => {
         name: 'Jane',
         age: 24,
         email: 'jane@example.com',
-        timestamp: '2022-02-02T12:34:56.789Z',
+        timestamp: '2023-07-02T12:34:56.789Z',
       },
       {
         id: 3,
         name: 'Bob',
         age: 25,
         email: 'bob@example.com',
-        timestamp: '2022-03-03T12:34:56.789Z',
+        timestamp: '2023-07-03T12:34:56.789Z',
       },
     ]);
   });
 
   it('should sort items by age in descending order', () => {
     const { result } = renderHook(() => useSortableData(items));
-    result.current.requestSort('age');
-    result.current.requestSort('age');
+    act(() => {
+      result.current.requestSort('age');
+    });
+    act(() => {
+      result.current.requestSort('age');
+    });
 
     expect(result.current.items).toEqual([
       {
@@ -66,14 +70,14 @@ describe('useSortableData', () => {
         name: 'Bob',
         age: 25,
         email: 'bob@example.com',
-        timestamp: '2022-03-03T12:34:56.789Z',
+        timestamp: '2023-07-03T12:34:56.789Z',
       },
       {
         id: 2,
         name: 'Jane',
         age: 24,
         email: 'jane@example.com',
-        timestamp: '2022-02-02T12:34:56.789Z',
+        timestamp: '2023-07-02T12:34:56.789Z',
       },
       {
         id: 1,
@@ -87,13 +91,15 @@ describe('useSortableData', () => {
 
   it('should filter items by timeframe', () => {
     const { result } = renderHook(() => useSortableData(items));
-    result.current.resetSort({
-      key: 'timestamp',
-      direction: 'ascending',
-      filter: {
-        timeframe: timeFrames.LASTMONTH,
+    act(() => {
+      result.current.resetSort({
         key: 'timestamp',
-      },
+        direction: 'ascending',
+        filter: {
+          timeframe: timeFrames.LASTMONTH,
+          key: 'timestamp',
+        },
+      });
     });
 
     expect(result.current.items).toEqual([
@@ -102,27 +108,29 @@ describe('useSortableData', () => {
         name: 'Jane',
         age: 24,
         email: 'jane@example.com',
-        timestamp: '2022-02-02T12:34:56.789Z',
+        timestamp: '2023-07-02T12:34:56.789Z',
       },
       {
         id: 3,
         name: 'Bob',
         age: 25,
         email: 'bob@example.com',
-        timestamp: '2022-03-03T12:34:56.789Z',
+        timestamp: '2023-07-03T12:34:56.789Z',
       },
     ]);
   });
   it('should filter items by a custom filter function', () => {
     const { result } = renderHook(() => useSortableData(items));
     const customFilter = (item) => item.name === 'Jane';
-    result.current.resetSort({
-      key: 'id',
-      direction: 'ascending',
-      filter: {
-        filterFcn: customFilter,
-      },
-    });
+    act(() =>
+      result.current.resetSort({
+        key: 'id',
+        direction: 'ascending',
+        filter: {
+          filterFcn: customFilter,
+        },
+      })
+    );
 
     expect(result.current.items).toEqual([
       {
@@ -130,7 +138,7 @@ describe('useSortableData', () => {
         name: 'Jane',
         age: 24,
         email: 'jane@example.com',
-        timestamp: '2022-02-02T12:34:56.789Z',
+        timestamp: '2023-07-02T12:34:56.789Z',
       },
     ]);
   });
