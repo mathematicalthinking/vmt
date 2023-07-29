9b2358e2afec0863911d34d6071b45c550e35f06
Irv Katz <60225276+exidy80@users.noreply.github.com>
2023-07-29 11:02:41 -0400

    make dates dynamic for 3rd test

--- client/__tests__/useSortableData.test.js
+++ client/__tests__/useSortableData.test.js
@@ -89,8 +89,57 @@ describe('useSortableData', () => {
     ]);
   });
 
+  const currentDate = new Date();
+
+  const dynamicItems = [
+    {
+      id: 1,
+      name: 'John',
+      age: 23,
+      email: 'john@example.com',
+      timestamp: new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth() - 2,
+        1
+      ).toISOString(), // 2 months ago
+    },
+    {
+      id: 2,
+      name: 'Jane',
+      age: 24,
+      email: 'jane@example.com',
+      timestamp: new Date(
+        currentDate.getTime() - 21 * 24 * 60 * 60 * 1000
+      ).toISOString(), // 3 weeks ago
+    },
+    {
+      id: 3,
+      name: 'Bob',
+      age: 25,
+      email: 'bob@example.com',
+      timestamp: currentDate.toISOString(), // now
+    },
+  ];
+
+  const expectedItems = [
+    {
+      id: 2,
+      name: 'Jane',
+      age: 24,
+      email: 'jane@example.com',
+      timestamp: dynamicItems[1].timestamp,
+    },
+    {
+      id: 3,
+      name: 'Bob',
+      age: 25,
+      email: 'bob@example.com',
+      timestamp: dynamicItems[2].timestamp,
+    },
+  ];
+
   it('should filter items by timeframe', () => {
-    const { result } = renderHook(() => useSortableData(items));
+    const { result } = renderHook(() => useSortableData(dynamicItems));
     act(() => {
       result.current.resetSort({
         key: 'timestamp',
@@ -102,23 +151,9 @@ describe('useSortableData', () => {
       });
     });
 
-    expect(result.current.items).toEqual([
-      {
-        id: 2,
-        name: 'Jane',
-        age: 24,
-        email: 'jane@example.com',
-        timestamp: '2023-07-02T12:34:56.789Z',
-      },
-      {
-        id: 3,
-        name: 'Bob',
-        age: 25,
-        email: 'bob@example.com',
-        timestamp: '2023-07-03T12:34:56.789Z',
-      },
-    ]);
+    expect(result.current.items).toEqual(expectedItems);
   });
+
   it('should filter items by a custom filter function', () => {
     const { result } = renderHook(() => useSortableData(items));
     const customFilter = (item) => item.name === 'Jane';
