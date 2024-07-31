b726f9097cd8ad1b0a9766d5b8e438135000a330
Irv Katz <60225276+exidy80@users.noreply.github.com>
2024-07-31 12:02:15 -0400

    Revert "timestamp login via client request"

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,6 +1,6 @@
 export { default as API } from './apiRequests';
 export * from './appUrls';
-export { default as AUTH } from './auth';
+export { default as auth } from './auth';
 export { default as buildLog } from './buildLog';
 export { default as COLOR_MAP } from './colorMap';
 export { default as createMongoId } from './createMongoId';
