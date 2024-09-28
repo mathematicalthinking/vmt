5fcd4b9eb8c30365b502d02def2e9dd83ad9b0b3
Irv Katz <60225276+exidy80@users.noreply.github.com>
2024-09-28 13:10:48 -0400

    env was mangled to string

--- client/scripts/build.js
+++ client/scripts/build.js
@@ -1,5 +1,6 @@
 'use strict';
 // Do this as the first thing so that any code reading it knows the right env.
+// @NOTE: All process.env variables get changed to strings at some point.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 process.env.REACT_APP_BUILD_DATE = new Date();
